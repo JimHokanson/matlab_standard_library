1bf9ca4f4fe23adcf93c37a60fe58567b3a69bee
Jim Hokanson <jim.hokanson@gmail.com>
2013-07-05 06:38:33 -0400

    Changed calling form of sl.cellstr.join.m NOT BACKWARDS COMPATIBLE

--- +sl/+cellstr/join.m
+++ +sl/+cellstr/join.m
@@ -1,39 +1,48 @@
-function str = join(cellstr_input,delimiter,treat_delimiter_as_literal)
+function str = join(cellstr_input,varargin)
 %toString
 %
-%   str = sl.cellstr.join(cellstr_input,*delimiter,*treat_delimiter_as_literal)
+%   str = sl.cellstr.join(cellstr_input,varargin)
 %
 %   INPUTS
 %   =======================================================================
-%   cellstr   : A cell array of strings to combine
-%   delimiter : (default ','), string to use in combining
-%   treat_delimiter_as_literal : (default false), e.g. normally \t is
-%           treated as a tab, but if prop is true, then it is literally '\t'
+%   cellstr   : A cell array of strings to combine.
+%   
+%   OPTIONAL INPUTS
+%   =======================================================================
+%   d            : (default ','), delimiter string to use in combining strings
+%   
+%       To treat a delimiter as a literal escape the backlash with a
+%       backlash. For example, this '\\t' will join strings with '\t'
+%       instead of a tab. Percents should be escaped with a percent.
+%   
+%       The final delimiter => sprintf(delim)
 %
+%   remove_empty : (default false), if true empty values are removed
+%   
 %   EXAMPLES
 %   =======================================================================
 %
 %   TODO: Finish Documentation
 
-if nargin < 2 || isempty(delimiter)
-    delimiter = ',';
-end
-
-if nargin < 3
-    treat_delimiter_as_literal = false;
-end
+in.d        = ',';
+in.remove_empty = false;
+in = sl.in.processVarargin(in,varargin);
 
 if isempty(cellstr_input)
     str = '';
 elseif ~iscell(cellstr_input)
     error('Input to %s must be a cell array',mfilename)
 else
-    P = cellstr_input(:)' ;
-    if treat_delimiter_as_literal
-        P(2,:) = {delimiter};
-    else
-        P(2,:) = {sprintf([delimiter '%s'],'')} ;  %Added on printing to handle things like \t and \n
+    P = cellstr_input(:)';
+    if in.remove_empty
+       P(cellfun('isempty',P)) = []; 
+       if isempty(P)
+          str = '';
+          return
+       end
     end
+    P(2,:) = {sprintf(in.d)} ;  %Added on printing to handle things like \t and \n
+    
     P{2,end} = [] ;
     str = sprintf('%s',P{:});
 end
