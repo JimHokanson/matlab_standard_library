7592d5819a80bbfc4efeb02fdcb3ba68f702293b
Jim Hokanson <jim.hokanson@gmail.com>
2013-07-17 09:10:55 -0400

    sprintf now allows single values for some inputs

--- +sl/+cellstr/sprintf.m
+++ +sl/+cellstr/sprintf.m
@@ -3,7 +3,12 @@ function output = sprintf(format,varargin)
 %
 %   output = sl.cellstr.sprintf(format,varargin)
 %
-%   varargin should be arrays or cell arrays of the same size
+%   varargin should be arrays or cell arrays of the same size OR singular
+%   elements.
+%
+%   NOTE: Strings are automatically converted to singular cells. True
+%   character arrays need to be converted to cells via num2cell.
+%
 %   See examples below ...
 %
 %   EXAMPLES:
@@ -19,30 +24,46 @@ function output = sprintf(format,varargin)
 %
 %   IMPROVEMENTS:
 %   =======================================================================
-%   1) Allow singular inputs as well ...
 %
+%
+mask = cellfun('isclass',varargin,'char');
+if any(mask)
+   I = find(mask);
+   for iChar = 1:length(I)
+      cur_index = I(iChar);
+      varargin{cur_index} = varargin(cur_index); 
+   end
+end
 
 len_all = cellfun('prodofsize',varargin);
-if ~all(len_all == len_all(1))
+if ~all(len_all == max(len_all) | len_all == 1)
     error('All variable inputs must have the same length')
 end
 
-nStrings = len_all(1);
+n_strings = max(len_all);
 %NOTE: I have at least one case that relies on this being true ...
 %Not sure what happens if others differ ...
 output = cell(size(varargin{1}));
 
-nParams = length(varargin);
-input_params  = cell(1,nParams);
+n_params      = length(varargin);
+input_params  = cell(1,n_params);
 
-for iStr = 1:nStrings
-    for iParam = 1:nParams
+for iStr = 1:n_strings
+    for iParam = 1:n_params
        if iscell(varargin{iParam})
           %Assigning cell to cell
-          input_params(iParam) = varargin{iParam}(iStr);
+          if len_all(iParam) == 1
+              input_params(iParam) = varargin{iParam};
+          else
+              input_params(iParam) = varargin{iParam}(iStr);
+          end
        else
-          %Careful: notice assigning numeric to cell
-          input_params{iParam} = varargin{iParam}(iStr);
+          if len_all(iParam) == 1 
+              input_params(iParam) = varargin(iParam);
+          else
+             %Careful: notice assigning numeric to cell
+              input_params{iParam} = varargin{iParam}(iStr); 
+          end
        end
     end 
     output{iStr} = sprintf(format,input_params{:});
