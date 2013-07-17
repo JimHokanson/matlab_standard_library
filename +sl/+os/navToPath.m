f7057aa7e850cba4c765620181e5de5436ceadd9
Jim Hokanson <jim.hokanson@gmail.com>
2013-07-17 09:10:22 -0400

    Documentation Update

--- +sl/+os/navToPath.m
+++ +sl/+os/navToPath.m
@@ -1,22 +1,41 @@
 function navToPath(file_or_folder_path)
+%navToPath  Opens os file-viewer to folder or file
 %
 %   sl.os.navToPath(file_or_folder_path)
 %
+%   Opens the specified file or folder using a file-system viewer (i.e.
+%   Windows explorer, Apple navigator??, etc.) 
+%
+%   Files are shown in their containing folder. 
+%
+%   Folders are opened directly (not shown in their parent). 
+%
+%   INPUT
+%   =======================================================================
+%   file_or_folder_path : The path to a file or a folder.
 %
 %   IMPROVEMENTS:
 %   =======================================================================
 %   1) Documentation
 %   2) File support for unix and macs
 %   3) Provide root support - show folder in parent folder
+%
+%   See Also:
+%   winopen
+
+%in.show_folder_in_parent = true;
+%in = sl.in.processVarargin(in,varargin);
 
 %Resolution to file or folder
 %--------------------------------------------------------------------------
+%NOTE: exist dir tests for file or folder :/
+%We need to first test for a file, then test for a folder ...
 if exist(file_or_folder_path,'file')
     file_path = file_or_folder_path;
-    is_file = true;
+    is_file   = true;
 elseif exist(file_or_folder_path,'dir')
     folder_path = file_or_folder_path;
-    is_file = false;
+    is_file     = false;
 else
     error('Specified file/folder not found')
 end
@@ -24,7 +43,11 @@ end
 if is_file
     if ispc
         %http://support.microsoft.com/kb/152457
-        system(['explorer.exe /select,' file_path]) 
+        %There are other options available ...
+        %NOTE: winopen will open the file using the default application
+        %which is not what we want ...s
+        system(['explorer.exe /select,' file_path]);
+
     else
         error('Function not yet expanded to support current os')
     end
@@ -34,6 +57,7 @@ else
     elseif ismac
         system(['open ' folder_path]);
     elseif isunix
+        %?? - from Chris
         [~,res] = system('which nautilus');
         if ~isempty(strfind(res,'no'))
             formattedWarning('Could not find Nautilus')
