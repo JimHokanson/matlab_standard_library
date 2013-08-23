5aff291e3948fa2344c78df3a04b67e2e25e16fa
Jim Hokanson <jim.hokanson@gmail.com>
2013-08-23 12:19:10 -0400

    Almost done improving speed to parseJSON

--- +sl/+io/parseJSON.m
+++ +sl/+io/parseJSON.m
@@ -2,281 +2,308 @@ function data = parseJSON(string)
 %
 %
 %   data = parseJSON(string)
-%
-% This function parses a JSON string and returns a cell array with the
-% parsed data. JSON objects are converted to structures and JSON arrays are
-% converted to cell arrays.
-%
-%   Original Version by:
-%   F. Glineur, 2009
-%   LICENSE: see http://www.mathworks.com/matlabcentral/fileexchange/23393
-%
-%   JAH NOTE: I've since modified the code a bit ...
 
-%JAH MOD
 
-persistent special_chars
+persistent SPECIAL_CHARS_SORTED
 
-if isempty(special_chars)
-    special_chars = helper__initializeSpecialChars();
+if isempty(SPECIAL_CHARS_SORTED)
+   SPECIAL_CHARS_SORTED = sort('"",:[]{}'); 
 end
 
-pos = 1;
-len = length(string);
-% String delimiters and escape characters are identified beforehand to improve speed
-esc       = regexp(string, '["\\]');
-index_esc = 1;
-len_esc   = length(esc);
+[is_string,is_esc_char,is_quote] = helper__getQuotesAndEscapes(string);
+
+[is_esc_char,string]   = helper__fixEscapes(is_esc_char,string);
 
 
-is_esc_char   = string == '\';
-is_quote_char = string == '"';
-quote_char_I  = find(is_quote_char);
-next_quote_I(is_quote_char) = [quote_char_I(2:end) length(string)+1];
+str_I      = find(is_quote);
+str_starts = str_I(1:2:end)+1;
+str_ends   = str_I(2:2:end)-1;
 
-%MEMORY ASSUMPTION: Tradeoff ...
-%For a given string, if it is a space, point to the next non-space
-%otherwise, point to itself
-% % % mask = [~isspace(string) true];
-% % % I    = fliplr(find(mask));
-% % % temp = cumsum(mask(end:-1:1));
-% % % next_non_space = I(temp(end:-1:1));
+n_str = length(str_starts);
+all_strings = cell(1,n_str);
 
+%TODO:
+%1) empty
+%2) not-empty but escaped
+%3) normal
 
-switch(next_char)
+is_empty_string = str_starts == str_ends + 1;
+
+has_escape = false(1,n_str);
+[~,bin] = histc(find(is_esc_char),str_I);
+
+%NOTE: If this fails, then our escape characters
+%are not-between the strings ...
+has_escape((bin+1)/2) = true;
+
+all_strings(is_empty_string) = {''};
+
+I_normal = find(~is_empty_string & ~has_escape);
+I_escape = find(~is_empty_string & has_escape);
+
+for iString = I_normal
+    all_strings{iString} = string(str_starts(iString):str_ends(iString));
+end
+
+for iString = I_escape
+    cur_indices = str_starts(iString):str_ends(iString);
+    cur_indices(is_esc_char(cur_indices)) = [];
+    all_strings{iString} = string(cur_indices);
+end
+
+%A good check ...
+%any(is_esc_char & ~is_string)
+
+%[]
+%{}
+%:
+%""
+%,
+
+cur_string_index  = 1;
+%NOTE: Second input is sorted ...
+is_special_I      = find(sl.str.quick.ismemberCharSortedSecondInput(string,SPECIAL_CHARS_SORTED) & ~is_string);
+cur_special_index = 1;
+
+switch(string(is_special_I(1)))
     case '{'
-        data = parse_object;
+        data = parse_object(string,is_special_I,cur_special_index,all_strings,cur_string_index);
     case '['
-        data = parse_array;
+        data = parse_array(string,is_special_I,cur_special_index,all_strings,cur_string_index);
     otherwise
-        error_pos('Outer level structure must be an object or an array');
+        error('Outer level structure must be an object or an array')
+        %error_pos('Outer level structure must be an object or an array',string,pos);
 end
 
+end
 
-    function object = parse_object
-        %NOTE: An object is like a structure, with fields
-        parse_char('{');
-        object = [];
-        if next_char ~= '}'
-            while 1
-                str = parse_string;
-                if isempty(str)
-                    error_pos('Name of value at position %d cannot be empty');
-                end
-                parse_char(':');
-                val = parse_value;
-                object.(valid_field(str)) = val;
-                if next_char == '}'
-                    break;
-                end
-                parse_char(',');
-            end
-        end
-        parse_char('}');
+function [is_esc_char,string] = helper__fixEscapes(is_esc_char,string)
+
+persistent CHARS_TO_FIX
+
+if isempty(CHARS_TO_FIX)
+   CHARS_TO_FIX = sort('bfnrt');
+   %TODO: Should move escape chars up here as well ...
+end
+
+is_esc_char_I = find(is_esc_char)+1;
+if ~isempty(is_esc_char_I)
+%TODO: This most likely could be improved using ismember
+esc_chars   = string(is_esc_char_I);
+%\" - do nothing
+%\\ - do nothing
+%\/ - do nothing
+
+[is_present,loc] = sl.str.quick.ismemberCharSortedSecondInput(esc_chars,CHARS_TO_FIX);
+
+if any(is_present)
+    fixed_chars = sprintf('\b\f\n\r\t');
+    string(is_esc_char_I(is_present)) = fixed_chars(loc(is_present));
+end
+
+I_unicode = find(esc_chars == 'u');
+n_unicode = length(I_unicode);
+if n_unicode ~= 0
+    all_unicode = char(zeros(6,n_unicode));
+    all_unicode(1,:) = '\';
+    all_unicode(2,:) = 'u';
+    unicode_starts = is_esc_char_I(I_unicode);
+    for iUni = 1:n_unicode
+        cur_start = unicode_starts(iUni);
+        all_unicode(3:end,iUni) = string(cur_start+1:cur_start+4);
+        
+        %We mark the 4 characters for omission in the grab
+        is_esc_char(cur_start+1:cur_start+4) = true;
+        %\u00a0
     end
+    
+    %NOTE: We assign the final value to u, the rest are marked for deletion
+    string(unicode_starts) = sl.str.javaStringToChar(org.apache.commons.lang.StringEscapeUtils.unescapeJava(all_unicode(:)'));
+    
+end
+end
 
-    function object = parse_array
-        parse_char('[');
-        object = {};
-        if next_char ~= ']'
-            while 1
-                val = parse_value;
-                object{end+1} = val;
-                if next_char == ']'
-                    break;
-                end
-                parse_char(',');
-            end
+end
+
+function [is_string,is_esc_char,is_quote] = helper__getQuotesAndEscapes(string)
+% % % % str_len = length(string);
+% % % % %NOTE: This could easily be mexed ...
+% % % % %---------------------------------------------------
+% % % % tic
+% % % % is_quote        = false(1,str_len);
+% % % % is_not_esc_char = true(1,str_len);
+% % % % for iStr = 2:length(string)
+% % % %     if is_not_esc_char(iStr-1)
+% % % %         if string(iStr) == '"'
+% % % %             is_quote(iStr) = true;
+% % % %         elseif string(iStr) == '\'
+% % % %             is_not_esc_char(iStr) = false;
+% % % %         end
+% % % %     end
+% % % % end
+% % % % is_esc_char = ~is_not_esc_char;
+% % % % toc
+% % % % 
+% % % % 
+% % % % tic
+[is_string,is_esc_char,is_quote] = parse_json_helper(string);
+
+
+end
+
+function [object,cur_special_index,cur_string_index] = parse_object(string,is_special_I,cur_special_index,all_strings,cur_string_index)
+
+%NOTE: An object is like a structure, with fields
+
+cur_special_index = cur_special_index + 1;
+
+object = [];
+
+if string(is_special_I(cur_special_index)) ~= '}'
+    while 1
+        
+        str = all_strings{cur_string_index};
+        cur_special_index = cur_special_index + 2;
+        cur_string_index  = cur_string_index  + 1;
+
+        
+%We'll let this cause an error below ...
+%         if isempty(str)
+%             error_pos('Name of value at position %d cannot be empty');
+%         end
+        
+        if string(is_special_I(cur_special_index)) ~= ':'
+            error('Expected : following object definition, observed: %s',string(is_special_I(cur_special_index)))
         end
-        parse_char(']');
-    end
-
-    function parse_char(c)
-        %Let's assume we're there already, this saves time if right
-        if string(pos) == c
-            pos = pos + 1;
-            skip_whitespace;
+        
+        cur_special_index = cur_special_index + 1;
+        
+        [val,cur_special_index,cur_string_index] = parse_value(string,is_special_I,cur_special_index,all_strings,cur_string_index);
+        
+        try
+            object.(str) = val;
+        catch
+            object.(valid_field(str)) = val;
+        end
+        
+        
+        if string(is_special_I(cur_special_index)) == '}'
+            break
         else
-            skip_whitespace;
-            if pos > len || string(pos) ~= c
-                error_pos(sprintf('Expected %c at position %%d', c));
-            else
-                pos = pos + 1;
-                skip_whitespace;
-            end
+            %NOTE: We should check that we have a comma ...
+            cur_special_index = cur_special_index + 1;
         end
     end
+end
 
-    function c = next_char
-        skip_whitespace;
-        if pos > len
-            c = [];
+cur_special_index = cur_special_index + 1;
+
+end
+
+function [object,cur_special_index,cur_string_index] = parse_array(string,is_special_I,cur_special_index,all_strings,cur_string_index)
+
+cur_special_index = cur_special_index + 1;
+
+%Yikes, this approach makes true array growth awful, could be improved with
+%preprocessing ...
+
+object = {};
+if string(is_special_I(cur_special_index)) ~= ']'
+    while 1
+        [val,cur_special_index,cur_string_index] = parse_value(string,is_special_I,cur_special_index,all_strings,cur_string_index);
+        
+        object{end+1} = val; %#ok<AGROW>
+        
+        if string(is_special_I(cur_special_index)) == ']'
+            break
         else
-            c = string(pos);
+            %NOTE: We should check that we have a comma ...
+            cur_special_index = cur_special_index + 1;
         end
     end
+end
 
-    function skip_whitespace
-        %JAH MOD
-        %pos = next_non_space(pos);
-        while pos <= len && isspace(string(pos))
-            pos = pos + 1;
-        end
-    end
+cur_special_index = cur_special_index + 1;
 
-    function str = parse_string
-        %
-        %
-        %   A string consists of characters
-        %   ---------------------------------------------
-        %   \" \\  \/  \b  \f  \n  \r  \uxxxx
-        %   What is \/ ->
-        
-        if string(pos) ~= '"'
-            error_pos('String starting with " expected at position %d');
-        end
-        
-        temp = next_quote_I(pos);
-        
-        if ~any(is_esc_char(pos+1:temp-1))
-            str = string(pos+1:temp-1);
-            if isempty(str)
-                str = '';
-            end
-            pos = temp + 1;
-            return
-        end
-        
-        %NOTE: At this point, we should only proceed
-        %if we have escape characters in the string
-        
-        pos = pos + 1;
-        
-        str  = '';
-        done = false;
-        while ~done
-            %Advance to next escape character ...
-            while index_esc <= len_esc && esc(index_esc) < pos
-                index_esc = index_esc + 1;
-            end
-            if index_esc > len_esc
-                str = [str string(pos:end)];
-                pos = len + 1;
-            else
-                str = [str string(pos:esc(index_esc)-1)];
-                pos = esc(index_esc);
-                esc_char = string(pos);
-                
-                if esc_char == '"'
-                    pos = pos + 1;
-                    %Yikes, this is the normal escape mode ...
-                    done = true;
-                elseif esc_char == '\'
-                    %Safety off ...
-                    % if pos+1 > len
-                    %     error_pos('End of file reached right after escape character');
-                    % end
-                    pos = pos + 1;
-                    next_char = string(pos);
-                    if next_char == 'u'
-                        %Safety off ...
-                        str(end+1) = helper__decodeJavaString(string(pos-1:pos+4));
-                        %error_pos('End of file reached in escaped unicode character');
-                        pos = pos + 5;
-                    else
-                        str(end+1) = special_chars{string(pos)};
-                        pos = pos + 1;
-                    end
-                end
-            end
-        end
-        if ~done
-            error_pos('End of file while expecting end of string');
-        end
-    end
+end
 
-    function num = parse_number
-        [num, one, err, delta] = sscanf(string(pos:min(len,pos+20)), '%f', 1); % TODO : compare with json(pos:end)
-        if ~isempty(err)
-            error_pos('Error reading number at position %d');
-        end
-        pos = pos + delta-1;
-    end
+% % % function special_chars = helper__initializeSpecialChars()
+% % % 
+% % % all_chars = '"\/bfnrt';
+% % % special_chars = cell(1,max(all_chars));
+% % % special_chars{'\'} = '\';
+% % % special_chars{'/'} = '/';
+% % % special_chars{'"'} = '"';
+% % % special_chars{'b'} = sprintf('\b');
+% % % special_chars{'f'} = sprintf('\f');
+% % % special_chars{'n'} = sprintf('\n');
+% % % special_chars{'r'} = sprintf('\r');
+% % % special_chars{'t'} = sprintf('\t');
+% % % 
+% % % end
 
-    function val = parse_value
-        switch(string(pos))
-            case '"'
-                val = parse_string;
-                return;
-            case '['
-                val = parse_array;
-                return;
-            case '{'
-                val = parse_object;
-                return;
-            case {'-','0','1','2','3','4','5','6','7','8','9'}
-                val = parse_number;
-                return;
+function str = valid_field(str)
+% From MATLAB doc: field names must begin with a letter, which may be
+% followed by any combination of letters, digits, and underscores.
+% Invalid characters will be converted to underscores, and the prefix
+% "alpha_" will be added if first character is not a letter.
+
+if ~isletter(str(1))
+    str = ['alpha_' str];
+end
+
+%TODO: This can be optimized ...
+str(~isletter(str) & ~('0' <= str & str <= '9')) = '_';
+
+end
+
+function error_pos(msg,string,pos)
+poss = max(min([pos-15 pos-1 pos pos+20],len),1);
+if poss(3) == poss(2)
+    poss(3:4) = poss(2)+[0 -1];         % display nothing after
+end
+msg = [sprintf(msg, pos) ' : ... ' string(poss(1):poss(2)) '<error>' string(poss(3):poss(4)) ' ... '];
+ME = MException('JSONparser:invalidFormat', msg);
+throw(ME);
+end
+
+function [val,cur_special_index,cur_string_index] = parse_value(string,is_special_I,cur_special_index,all_strings,cur_string_index)
+
+%[ - start of an array
+%" - start of a string
+%{ - start of an object
+%
+%--- following indicate a number or special value, true, false, null
+%]
+%}
+%,
+
+
+switch string(is_special_I(cur_special_index))
+    case '"'
+        val = all_strings{cur_string_index};
+        cur_special_index = cur_special_index + 2;
+        cur_string_index  = cur_string_index + 1;
+    case '['
+        [val,cur_special_index,cur_string_index] = parse_array(string,is_special_I,cur_special_index,all_strings,cur_string_index);
+    case '{'
+        [val,cur_special_index,cur_string_index] = parse_object(string,is_special_I,cur_special_index,all_strings,cur_string_index);
+    otherwise
+        %Pointer to ',' ']' or '}'
+        %??? - use deblank instead, don't care about trailing stuffs ...
+        prev_str = strtrim(string((is_special_I(cur_special_index-1)+1):(is_special_I(cur_special_index)-1)));
+        switch(prev_str(1))
             case 't'
-                if pos+3 <= len && strcmpi(string(pos:pos+3), 'true')
-                    val = true;
-                    pos = pos + 4;
-                    return;
-                end
+                val = true;
             case 'f'
-                if pos+4 <= len && strcmpi(string(pos:pos+4), 'false')
-                    val = false;
-                    pos = pos + 5;
-                    return;
-                end
+                val = false;
             case 'n'
-                if pos+3 <= len && strcmpi(string(pos:pos+3), 'null')
-                    val = [];
-                    pos = pos + 4;
-                    return;
-                end
+                val = [];
+            otherwise
+                %TODO: Add error check ???
+                val = sscanf(prev_str,'%f',1);
+                %Treat as number
         end
-        error_pos('Value expected at position %d');
-    end
-
-    function error_pos(msg)
-        poss = max(min([pos-15 pos-1 pos pos+20],len),1);
-        if poss(3) == poss(2)
-            poss(3:4) = poss(2)+[0 -1];         % display nothing after
-        end
-        msg = [sprintf(msg, pos) ' : ... ' string(poss(1):poss(2)) '<error>' string(poss(3):poss(4)) ' ... '];
-        ME = MException('JSONparser:invalidFormat', msg);
-        throw(ME);
-    end
-
-    function str = valid_field(str)
-        % From MATLAB doc: field names must begin with a letter, which may be
-        % followed by any combination of letters, digits, and underscores.
-        % Invalid characters will be converted to underscores, and the prefix
-        % "alpha_" will be added if first character is not a letter.
-        if ~isletter(str(1))
-            str = ['alpha_' str];
-        end
-        str(~isletter(str) & ~('0' <= str & str <= '9')) = '_';
-    end
-
 end
-
-function char_out = helper__decodeJavaString(char_in)
-char_out = sl.str.javaStringToChar(org.apache.commons.lang.StringEscapeUtils.unescapeJava(char_in));
-end
-
-function special_chars = helper__initializeSpecialChars()
-
-all_chars = '"\/bfnrt';
-special_chars = cell(1,max(all_chars));
-special_chars{'\'} = '\';
-special_chars{'/'} = '/';
-special_chars{'"'} = '"';
-special_chars{'b'} = sprintf('\b');
-special_chars{'f'} = sprintf('\f');
-special_chars{'n'} = sprintf('\n');
-special_chars{'r'} = sprintf('\r');
-special_chars{'t'} = sprintf('\t');
 
 end
