4828e01acb7628f442307fc74d9ce19cfdba406c
Jim Hokanson <jim.hokanson@gmail.com>
2014-05-06 16:02:15 -0400

    Adding stack class

---
+++ +sl/stack.m
@@ -0,0 +1,31 @@
+classdef (Hidden) stack
+    %
+    
+    properties
+    end
+    
+    methods (Hidden,Static)
+        function goDebugHelper(stack_printout)
+            %
+            %   sl.stack.goDebugHelper(stack_printout)
+            lines = sl.str.getLines(stack_printout);
+            
+            [~,is_matched] = sl.cellstr.regexpSingleMatchTokens(...
+                lines,'^>','output_type','match');
+            
+            I = find(is_matched,1);
+            
+            if isempty(I)
+                %TODO: Throw warning
+                idx_use = 2;
+            else
+                idx_use = I + 1; %We need to add 
+            end
+            
+            s = dbstack('-completenames');
+            matlab.desktop.editor.openAndGoToLine(s(idx_use).file,s(idx_use).line); 
+        end
+    end
+    
+end
+
