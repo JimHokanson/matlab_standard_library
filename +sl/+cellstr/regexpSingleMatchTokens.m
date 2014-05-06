96c539da9af929385451b902b6e5e6bbff896d6f
Jim Hokanson <jim.hokanson@gmail.com>
2014-05-06 16:06:51 -0400

    Updated cellstr.regexpSingleMatchTokens to allow different output types, maybe should change name?

--- +sl/+cellstr/regexpSingleMatchTokens.m
+++ +sl/+cellstr/regexpSingleMatchTokens.m
@@ -1,4 +1,4 @@
-function [output,is_matched] = regexpSingleMatchTokens(data_in,pattern)
+function [output,is_matched] = regexpSingleMatchTokens(data_in,pattern,varargin)
 %
 %
 %   [output,is_matched] = sl.cellstr.regexpSingleMatchTokens(data_in,pattern)
@@ -22,6 +22,8 @@ function [output,is_matched] = regexpSingleMatchTokens(data_in,pattern)
 %     'Krnjevic '                       '1992'                         [1x53  char]
 %     'Lan, Crago '                     '1992'                         [1x65  char]
 
+in.output_type = 'tokens';
+in = sl.in.processVarargin(in,varargin);
 
 if ischar(data_in)
     error('This function is designed for cell strings')
@@ -29,7 +31,7 @@ end
 
 %??? Provide length as input????
 
-temp = regexp(data_in,pattern,'tokens','once');
+temp = regexp(data_in,pattern,in.output_type,'once');
 
 len = cellfun('length',temp);
 
@@ -37,15 +39,36 @@ is_matched = len ~= 0;
 
 output_len = max(len);
 
-%TODO: Don't iterate over non-matches
-%Do this assignment on the output
-temp(~is_matched) = {repmat({''},1,output_len)};
-
 n_rows = length(data_in);
 
-output = cell(n_rows,output_len);
-
-for iRow = 1:n_rows
-   output(iRow,:) = temp{iRow}; 
+switch in.output_type
+    case 'tokens'
+        output = cell(n_rows,output_len);
+        output(~is_matched,:) = {''};
+        for iRow = find(is_matched)
+           output(iRow,:) = temp{iRow};  
+        end
+    case 'match'
+        output = cell(n_rows,1);
+        output(~is_matched,:) = {''};
+        output(is_matched) = temp(is_matched);
+%     case 'start'
+%         type = 'numeric';
+    otherwise
+        error('Output type: "%s" not yet supported')
 end
+
+
+
+% % % %TODO: Don't iterate over non-matches
+% % % %Do this assignment on the output
+% % % temp(~is_matched) = {repmat({''},1,output_len)};
+% % % 
+% % % n_rows = length(data_in);
+% % % 
+% % % output = cell(n_rows,output_len);
+% % % 
+% % % for iRow = 1:n_rows
+% % %    output(iRow,:) = temp{iRow}; 
+% % % end
 
