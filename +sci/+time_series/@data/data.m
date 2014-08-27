7fe6e0e47e3a6d834ae3b892389202631ff36ae5
JimHokanson <jim.hokanson@gmail.com>
2014-08-27 17:35:44 -0400

    Cleaning up data and time

--- +sci/+time_series/@data/data.m
+++ +sci/+time_series/@data/data.m
@@ -1,4 +1,4 @@
-classdef data < sl.obj.handle_light
+classdef data < sl.obj.display_class
     %
     %   Class:
     %   sci.time_series.data
@@ -12,12 +12,17 @@ classdef data < sl.obj.handle_light
     %   - averaging to a stimulus
     
     properties
-        d    %numeric array This is the actual data. In general it is
-        %preferable to retrieve the data via:
+        d    %[samples x channels x repetitions]
         %
+        %   This is the actual data. In general it is preferable to retrieve
+        %   the data via:
+        %
+        %       obj.getRawDataAndTime()
+        
         time     %sci.time_series.time
         units
         n_channels
+        n_reps
     end
     
     %Add on properties ----------------------------------------------------
@@ -40,6 +45,17 @@ classdef data < sl.obj.handle_light
         %   which certain things occured. The idea was that we could use
         %   this information for plotting or data manipulation.
         
+    end
+    
+    properties (Dependent)
+        event_names
+    end
+    
+    %Dependent Methods ----------------------------------------------------
+    methods
+        function value = get.event_names(obj)
+            value = fieldnames(obj.devents);
+        end
     end
     
     %Constructor ----------------------------------------------------------
@@ -68,12 +84,13 @@ classdef data < sl.obj.handle_light
             %    data_in must be with samples going down the rows
             
             in.history = {};
-            in.units = 'Unknown';
+            in.units   = 'Unknown';
             in.channel_labels = ''; %TODO: If numeric, change to string ...
-            in.events = [];
+            in.events  = [];
             in = sl.in.processVarargin(in,varargin);
             
             obj.n_channels = size(data_in,2);
+            obj.n_reps     = size(data_in,3);
             
             obj.d = data_in;
             
@@ -95,7 +112,7 @@ classdef data < sl.obj.handle_light
         function plot(objs,local_options,plotting_options)
             %
             %
-            %   TODO: How do we want to plot multiple repetitions ...
+            
             %
             %   plot(obj,local_options,plotting_options)
             %
@@ -106,6 +123,11 @@ classdef data < sl.obj.handle_light
             %
             %   Plotting Options: cell array
             %   ----------------------------
+            
+            %TODO: Plotting multiple objects on the same figure is a
+            %problem as they may have completely different starting dates
+            %
+            %   TODO: How do we want to plot multiple repetitions ...
             
             if nargin < 2
                 local_options = {};
@@ -164,7 +186,7 @@ classdef data < sl.obj.handle_light
             end
             
             obj.history = [obj.history; history_elements];
-        end  
+        end
     end
     
     %Data changing --------------------------------------------------------
@@ -184,70 +206,117 @@ classdef data < sl.obj.handle_light
             df = sci.time_series.data_filterer('filters',filters);
             df.filter(obj,'subtract_filter_result',in.subtract_filter_result);
         end
+        function decimated_data = decimateData(objs,bin_width,varargin)
+            
+            for iObj = 1:length(objs)
+                cur_obj = objs(iObj);
+            keyboard
+           %sample_width = ceil(bin_width*obj.time.
+            
+            end
+            
+            
+        end
     end
     
     %Time related manipulations -------------------------------------------
     methods
-        function data_subset_objs = getDataSubset(objs,start_event,start_event_index,stop_event,stop_event_index)
-            %            
+        function data_subset_objs = getDataSubset(objs,start_event,start_event_index,stop_event,stop_event_index,varargin)
             %
+            %   Returns a new object that only has a subset of the data.
             %
-            %   
+            %   Calling Forms:
+            %   --------------
+            %   getDataSubset(objs,start_event,start_event_index,stop_event,stop_event_index,varargin)
+            %
+            %   getDataSubset(objs,start_time,[],stop_time,[],varargin)
+            %
+            %   Inputs:
+            %   -------
+            %
+            %   Optional Inputs:
+            %   ----------------
+            %   align_time_to_start : logical (default false)
+            %       If this value is true, the zero point of the time is
+            %       shifted such that
+            %
+            %   See Also:
+            %
             
+            in.align_time_to_start = false;
+            in = sl.in.processVarargin(in,varargin);
             
-            n_objs = length(objs);
-            temp_objs_ca = cell(1,n_objs);
-            for iObj = 1:n_objs
-               cur_obj = objs(iObj); 
-               
-               start_time = cur_obj.devents.(start_event).times(start_event_index);
-               end_time   = cur_obj.devents.(stop_event).times(stop_event_index);
-               
-               temp_objs_ca{iObj} = cur_obj.getDataAlignedToEvent(start_time,[0 end_time-start_time]);
+            if in.align_time_to_start
+                first_sample_time = 0;
+            else
+                first_sample_time = [];
             end
             
-            %Crap, this currently also shifts the time :/
+            n_objs = length(objs);
+            all_start_times = zeros(1,n_objs);
+            temp_objs_ca = cell(1,n_objs);
+            for iObj = 1:n_objs
+                cur_obj = objs(iObj);
+                
+                start_time = cur_obj.devents.(start_event).times(start_event_index);
+                end_time   = cur_obj.devents.(stop_event).times(stop_event_index);
+                
+                %TODO: Make this a function ...
+                start_index = h__timeToSamples(cur_obj,start_time);
+                end_index   = h__timeToSamples(cur_obj,end_time);
+                
+                new_data        = cur_obj.d(start_index:end_index,:,:);
+                
+                new_time_object = h__getNewTimeObject(cur_obj,start_index,end_index,'first_sample_time',first_sample_time);
+                
+                
+                temp_objs_ca{iObj} = sci.time_series.data(new_data,new_time_object);
+            end
             
             data_subset_objs = [temp_objs_ca{:}];
+            
+            if in.align_time_to_start
+                data_subset_objs.zeroTimeByEvent(all_start_times);
+            end
         end
         function zeroTimeByEvent(objs,event_name_or_time_array)
-           %
-           %    
-           %    Redefines time such that the time of event is now at time
-           %    zero.
-           %
-           %    objs.zeroTimeByEvent(event_name)
-           %
-           %    objs.zeroTimeByEvent(event_times)
-           %    
-           %    Inputs:
-           %    -------
-           %    event_name :
-           %        This refers to one of the internal events in the system. 
-           %    event_times :
-           %        A single event time should be provided for each object
-           
-           n_objects = length(objs);
-           if isnumeric(event_name_or_time_array)
-               event_times = event_name_or_time_array;
-           else
-               event_times = zeros(1,n_objects);
-               for iObj = 1:n_objects
-                   temp_event_obj = objs(iObj).devents.(event_name_or_time_array);
-                   if length(temp_event_obj.times) ~= 1
-                       error('Each event must have only 1 time value ..., for now')
-                   end
-                   event_times(iObj) = temp_event_obj.times;
-               end
-           end
-           
-           %TODO: Make this a method - shift time
-           for iObj = 1:n_objects
-              objs(iObj).time.start_offset = objs(iObj).time.start_offset - event_times(iObj); 
-           end
-           
-           %TODO: We need to zero the times in the events as well ...
-           
+            %
+            %
+            %    Redefines time such that the time of event is now at time
+            %    zero.
+            %
+            %    objs.zeroTimeByEvent(event_name)
+            %
+            %    objs.zeroTimeByEvent(event_times)
+            %
+            %    Inputs:
+            %    -------
+            %    event_name :
+            %        This refers to one of the internal events in the system.
+            %    event_times :
+            %        A single event time should be provided for each object
+            
+            n_objects = length(objs);
+            if isnumeric(event_name_or_time_array)
+                event_times = event_name_or_time_array;
+            else
+                event_times = zeros(1,n_objects);
+                for iObj = 1:n_objects
+                    temp_event_obj = objs(iObj).devents.(event_name_or_time_array);
+                    if length(temp_event_obj.times) ~= 1
+                        error('Each event must have only 1 time value ..., for now')
+                    end
+                    event_times(iObj) = temp_event_obj.times;
+                end
+            end
+            
+            %TODO: Make this a method - shift time
+            for iObj = 1:n_objects
+                objs(iObj).time.start_offset = objs(iObj).time.start_offset - event_times(iObj);
+            end
+            
+            %TODO: We need to zero the times in the events as well ...
+            
         end
         function event_aligned_data = getDataAlignedToEvent(obj,event_times,new_time_range,varargin)
             %
@@ -262,7 +331,8 @@ classdef data < sl.obj.handle_light
             %
             %   Optional Inputs:
             %   ----------------
-            %   allow_overlap:
+            %   allow_overlap:  Not yet implemented ...
+            %
             %
             %   Outputs:
             %   --------
@@ -294,6 +364,9 @@ classdef data < sl.obj.handle_light
             in = sl.in.processVarargin(in,varargin);
             
             [indices,time_errors] = obj.time.getNearestIndices(event_times);
+            
+            
+            %TODO: Use h__getNewTimeObject
             
             start_index_1 = h__timeToSamples(obj,event_times(1)+new_time_range(1));
             end_index_1   = h__timeToSamples(obj,event_times(1)+new_time_range(2));
@@ -330,11 +403,52 @@ classdef data < sl.obj.handle_light
             time = obj.time.getTimeArray();
         end
         function sample_number = timeToSample(obj)
-            
+           error('Not yet implemented') 
         end
     end
 end
 
+%Helper functions ---------------------------------------------------------
+function event_times = h__getEventTimes(obj,event_name,varargin)
+%
+%
+%   See Also:
+%   sci.time_series.time_events
+
+%TODO: Create public method that retrieves a particular event
+
+in.indices = 'all';
+in = sl.in.processVarargin(in,varargin);
+
+events = obj.devents;
+
+%TODO: Check for name
+if ~isfield(events,event_name)
+    error('Requested event: %s, does not exist',event_name)
+end
+
+event_obj = events.(event_name);
+
+if ischar(in.indices)
+    event_times = event_obj.times;
+else
+    %Might surround with try/catch
+    event_times = event_obj.times(in.indices);
+end
+
+end
 function samples = h__timeToSamples(obj,times)
-   samples = obj.time.getNearestIndices(times);
+samples = obj.time.getNearestIndices(times);
+end
+function new_time_object = h__getNewTimeObject(obj,first_sample,last_sample,varargin)
+
+in.first_sample_time = [];
+%empty - keeps its time
+%0 - first value will be zero
+in = sl.in.processVarargin(in,varargin);
+
+n_samples = last_sample - first_sample + 1;
+new_time_object = obj.time.getNewTimeObjectForDataSubset(first_sample,n_samples,...
+    'first_sample_time',in.first_sample_time);
+
 end
--- +sci/+time_series/time.m
+++ +sci/+time_series/time.m
@@ -15,7 +15,9 @@ classdef time < sl.obj.display_class
     %   thing.
     
     properties
-        start_datetime %This isn't really being used currently.
+        start_datetime %This can be used for real dates to identify the
+        %actual time of the first sample. No support for time zones is in
+        %place.
         
         start_offset = 0 %(s)
         dt %seconds
@@ -46,21 +48,39 @@ classdef time < sl.obj.display_class
             %
             %   obj = sci.time_series.time(dt,n_samples)
             %
+            %   Inputs:
+            %   -------
+            %   dt : 
+            %       Time between sample points.
+            %   n_samples : 
+            %       # of samples in the data.
+            %
             %   Optional Inputs:
             %   ----------------
-            %   start_offset:
+            %   start_datetime : datenum
+            %      Start of the data collection with date and time
+            %      information.
+            %   start_offset : (default 0)
+            %       Normally this will be 0.
+            %   sample_offset :
+            %       This can be specified instead of "start_offset" in
+            %       cases in which it is more natural to specify which
+            %       sample is being used.
             %
             %   See Also:
             %   sci.time_series.time.getNewTimeForDataSubset
             
             %TODO: Document these optional inputs
             %sample_offset - for when the
+            in.start_datetime = 0;
             in.start_offset = [];
             in.sample_offset = [];
             in = sl.in.processVarargin(in,varargin);
             
-            %TODO: I think I'd like to clean this up ...
             if ~isempty(in.sample_offset)
+                if ~isempty(in.start_offset)
+                    error('Incorrect function usage, only start_offset or sample_offset may be specified, not both')
+                end
                 obj.start_offset = dt*(in.sample_offset-1);
             elseif ~isempty(in.start_offset)
                 obj.start_offset = in.start_offset;
@@ -68,6 +88,7 @@ classdef time < sl.obj.display_class
                 obj.start_offset = 0;
             end
             
+            obj.start_datetime = in.start_datetime;
             obj.dt = dt;
             obj.n_samples = n_samples;
         end
@@ -110,8 +131,8 @@ classdef time < sl.obj.display_class
             %
             %   Outputs:
             %   --------
-            %   indices:
-            %   time_errors:
+            %   indices :
+            %   time_errors :
             
             raw_indices = (times - obj.start_offset)./obj.dt;
             indices = round(raw_indices)+1;
@@ -123,13 +144,49 @@ classdef time < sl.obj.display_class
     
     %Construction methods -------------------------------------------------
     methods
-        function new_time_object = getNewTimeObjectForDataSubset(obj,new_start_time,n_samples)
+        function new_time_object = getNewTimeObjectForDataSubset(obj,start_sample,n_samples,varargin)
             %
             %   new_time_object = obj.getNewTimeObjectForDataSubset(new_start_time,n_samples)
+            %   
+            %   Inputs:
+            %   -------
+            %   start_sample : 
+            %   n_samples : 
             %
+            %   Optional Inputs:
+            %   ----------------
+            %   first_sample_time : default (no change)
+            %       The default behavior is to keep the time offset of the 
+            %       sample. For example, with a sampling rate of 1 and a 
+            %       start offset of 0, the 1st sample occurs at time 2 and
+            %       the 2nd sample occurs at time 1. If we get a subset of
+            %       data starting with the 2nd sample, the new start offset
+            %       will be 1, such that the 2nd sample (now 1st) still
+            %       occurs at time t = 1
+            %
+            %   
+            %       
+            %   
+            %   What does this do?????
+            
+            in.first_sample_time = [];
+            in = sl.in.processVarargin(in,varargin);
+            
+            first_sample_real_time = (start_sample-1)*obj.dt + obj.start_offset;
+            
+            if isempty(in.first_sample_time)
+                start_offset = first_sample_real_time; %#ok<PROP>
+                start_datetime = obj.start_datetime; %#ok<PROP>
+            else
+                start_offset = in.first_sample_time; %#ok<PROP>
+                time_change = first_sample_real_time - start_offset;%#ok<PROP>
+                start_datetime = obj.start_datetime + time_change;%#ok<PROP>
+            end
+            
             new_time_object = sci.time_series.time(...
                 obj.dt,n_samples,...
-                'start_offset',new_start_time);
+                'start_offset',start_offset,... %#ok<PROP>
+                'start_datetime',start_datetime);%#ok<PROP>
         end
     end
     
