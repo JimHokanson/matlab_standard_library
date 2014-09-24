95971ffbce938de69f767ea9980b45c89fa71063
DanielHull <danielhull13@gmail.com>
2014-09-24 11:02:07 -0400

    Update on methods generating functional links
    This edit is almost done. Exact reference of Static is still needed.
    still working on…
    Daniel

--- editor.m
+++ editor.m
@@ -5,4 +5,4 @@
 %  2) adds on a link for a function prototype
 %  3) adds an indicator if the method is static (s)
 %  4) retrieve the line where the function is defined
-%  5) utilize a profiler that can make it faster.
+%  5) utilize a profiler that can make it faster.s
