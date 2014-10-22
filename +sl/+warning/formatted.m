bd0259a9b260fe172e32926bfa5a827c978cfb1c
JimHokanson <jim.hokanson@gmail.com>
2014-10-22 12:55:11 -0400

    Fixed bugs in sl.warning.formatted - added test class

--- +sl/+warning/formatted.m
+++ +sl/+warning/formatted.m
@@ -1,45 +1,69 @@
 function varargout = formatted(message_str,varargin)
-%formatted  Create a formatted warning with a link to the calling method
+%x  Create a formatted warning with a link to the calling method.
 %
+%   Calling forms:
+%   --------------
 %   sl.warning.formatted(message_str,**formatting_option_inputs)
 %   
 %   formatted_warning_str = sl.warning.formatted(message_str)
 %
-%   Prints a warning with an open to link link in the form of
-%   [WARNING <file>:<line> ] <message>
+%   Summary:
+%   --------
+%   The default behavior with no output argument is to print a warning
+%   of the form:
 %
-%   If an output is requested display is quenched.
+%       [WARNING <file>:<line> ] <message>
 %
-%   INPUTS
-%   =======================================================================
-%   
+%   With a link to the to line where the call came from.
 %
-% tags: text, display
-% see also: getCallingFunction, createOpenToLineLink, displayCallStack
+%   If an output is requested the display is quenched.
+%
+%   Inputs:
+%   -------
+%   message_str : string
+%       The warning message to print. It can also be a formatting string.
+%       See the examples for clarification.
+%   formatting_option_inputs : 
+%       When these are passed in the final warning message is computed via
+%       sprintf as:
+%           messageStr = sprintf(message_str,formatting_option_inputs{:});
+%
+%   Outputs:
+%   --------
+%   formatted_warning_str
+%
+%   Examples:
+%   ---------
+%   sl.warning.formatted('Surprisingly low channel count: %d',channel_count)
+%
+%   sl.warning.formatted('This code branch is in beta testing')
+%
+%   str = sl.warning.formatted('When does this code run???')
 
 if nargin > 1
-    messageStr = sprintf(message_str,varargin{:});
+    message_str = sprintf(message_str,varargin{:});
 end
 
 
 %CODE STRING
 %------------------------
-info = sl.stack.getCallingFunction;
+info = sl.stack.calling_function_info;
 
-link_txt = sprintf('%s.m:%d',info.name,info.line);
+link_txt = sprintf('%s.m:%d',info.name,info.line_number);
 % check if java is enabled ( it is usually on ), if it is print a nice link 
 % to the code, otherwise print the raw text.
 
 if usejava('desktop')
-    link_str = sl.cmd_window.createOpenToLineLink(file,line,link_txt);
+    link_str = sl.cmd_window.createOpenToLineLink(info.file_path,info.line_number,link_txt);
 else
     link_str = link_txt;
 end
 %---------------------------------------------------
 
-warning_str = sprintf('[WARNING %s] %s', link_str, messageStr);
+warning_str = sprintf('[WARNING %s] %s', link_str, message_str);
 if nargout < 1
-    NotifierManager.notify('warning',warning_str);
+    %NotifierManager.notify('warning',warning_str);
+    fprintf(2,'%s\n',warning_str);
 else
     varargout{1} = warning_str;
 end
---
+++ +sl/+warning/tests_formatted.m
@@ -0,0 +1,17 @@
+classdef (Hidden) tests_formatted
+    %
+    %   Class:
+    %   sl.warning.tests_formatted
+    
+    properties
+    end
+    
+    methods (Static)
+        function mainTest()
+            %sl.warning.tests_formatted.mainTest
+           sl.warning.formatted('Please work!') 
+        end
+    end
+    
+end
+
