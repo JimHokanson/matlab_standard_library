e3b23301475c42e2f8f0a6dba8b0a6d4dff5d8bd
JimHokanson <jim.hokanson@gmail.com>
2014-12-03 20:06:52 -0500

    moved getSubplotAxesHandles.m to +figure, no longer needed here

--- +sl/+plot/+postp/getSubplotAxesHandles.m
+++
@@ -1,7 +0,0 @@
-function getSubplotAxesHandles(fig_h)
-%
-%   sl.plot.postp.getSubplotAxesHandles(fig_h)
-%
-%   TODO: Implement this function
-%
-%   Idea: Get positions and infer numbering from positions
