caa28572d5ebaa3541f4eece76a2a314a44ba4f4
JimHokanson <jim.hokanson@gmail.com>
2014-12-16 15:43:03 -0500

    Added support for class instances to helpc

--- global_namespace_functions/helpc.m
+++ global_namespace_functions/helpc.m
@@ -1,7 +1,17 @@
-function helpc(class_name)
+function helpc(class_name_or_instance)
 %x Returns help for the constructor of a class, instead of the class itself
 %
+%   Calling Forms:
+%   --------------
+%   1)
 %   helpc(class_name)
+%   
+%   2)
+%   helpc class_name
+%
+%   3)
+%   helpc(class_instance)
+%
 %
 %   This function exists to clarify the difference between getting help for
 %   a constructor versus the class itself. 99% of the time, I want help for
@@ -18,8 +28,15 @@ function helpc(class_name)
 %   See Also:
 %   editc
 
+if isobject(class_name_or_instance)
+    class_name = class(class_name_or_instance);
+else
+    class_name = class_name_or_instance;
+end
+
 temp = sl.obj.getClassNameWithoutPackages(class_name);
 
+%This basically says get help for the constructor of the class
 help(sprintf('%s>%s',class_name,temp));
 
 end
