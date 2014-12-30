b39f9b6c176df1ece48ef17112dbb2ffe6055588
JimHokanson <jim.hokanson@gmail.com>
2014-12-30 14:36:23 -0500

    why isn't minMaxOfDataSubset.cpp working?

--- +sl/+array/mex_code/minMaxOfDataSubset.cpp
+++ +sl/+array/mex_code/minMaxOfDataSubset.cpp
@@ -66,6 +66,7 @@ void mexFunction( int nlhs, mxArray *plhs[], int nrhs, const mxArray *prhs[] )
     
     mwSize assignment_offset = -1;
     mwSize row_start, row_end, col_start, col_end;
+    mwSize n_values;
     
     double *init_offset;
     double *data_offset;
@@ -90,6 +91,10 @@ void mexFunction( int nlhs, mxArray *plhs[], int nrhs, const mxArray *prhs[] )
     double *xp, *xend, *minp, *maxp;
     double *all_max, *all_min, *all_max_I, *all_min_I;
     
+    double  *xr;
+    
+    mxArray *lhs[2], *x;
+    
     all_max   = (double *) mxGetData(plhs[0]);
     all_min   = (double *) mxGetData(plhs[1]);
     all_max_I = (double *) mxGetData(plhs[2]);
@@ -97,79 +102,72 @@ void mexFunction( int nlhs, mxArray *plhs[], int nrhs, const mxArray *prhs[] )
     
     mwSize n_rows = mxGetM(prhs[0]);
     mwSize n_cols = mxGetN(prhs[0]);
-// // // // //     
-// // // // //     if (dim_use == 1){
-    
-        //col_start   = (mwSize)(starts2[0]-1);
+
         data_offset = data; // + (row_start*n_rows);
+        
+        x =  mxCreateDoubleMatrix(1, 1, mxREAL);
+        
+        xr = mxGetPr(x);
+        //mxFree(mxGetPr(x))
+        
+        lhs[0] = mxCreateDoubleScalar(0);
+        lhs[1] = mxCreateDoubleScalar(1);
         
         for (mwSize iChan = 0; iChan < n_chans; iChan++){
 
             //We are looping over the subset of data which we are working with
             for (mwSize iGroup = 0; iGroup < n_groups; iGroup++){
-                row_start  = (mwSize) starts1[iGroup];
-                row_end    = (mwSize) ends1[iGroup];
+                row_start = (mwSize) starts1[iGroup];
                 
-                xp   = data_offset + row_start - 1;
-                xend = data_offset + row_end   - 1;
-                
-                minp = xp;
-                maxp = xp;
-                
-                while (xp < xend) {
-                    if (*xp > *maxp){
-                        maxp = xp;
-                    } else if (*xp < *minp) {
-                        minp = xp;
-                    }
-                    xp++;
-                }
+                xp = data_offset + row_start - 1;
+                //n_values = row_end - row_start + 1;
+                                
+                mxSetPr(x,xp);
+                mxSetM(x,1);
+                mxSetN(x,1);
                 
                 assignment_offset++;
-                //all_max[assignment_offset]   = *maxp;
-                //all_min[assignment_offset]   = *minp;
-                all_max_I[assignment_offset] = minp - data_offset + 1;
-                all_min_I[assignment_offset] = maxp - data_offset + 1;
+                mexCallMATLAB(2, lhs, 1, &x, "min");
+                all_min[assignment_offset] = *mxGetPr(lhs[0]);
+                all_min_I[assignment_offset] = *mxGetPr(lhs[1]);
                 
+                mexCallMATLAB(2, lhs, 1, &x, "max");
+                all_max[assignment_offset] = *mxGetPr(lhs[0]);
+                all_max_I[assignment_offset] = *mxGetPr(lhs[1]); 
+
             }
             data_offset += n_rows;
         }
-    
-    
-    
-// // // // //     } else { 
-// // // // // 
-// // // // //          
-// // // // // 
-// // // // // //grabbing chunks over 2nd dimension ...
-// // // // //         
-// // // // // //         for (mwSize iGroup = 0; iGroup < n_groups; iGroup++){
-// // // // // //             start1  = (mwSize) bounds1[iGroup][0];
-// // // // // //             end1    = (mwSize) bounds1[1][1];
-// // // // // //             start2  = (mwSize) bounds2[0];
-// // // // // //             end2    = (mwSize) bounds2[1];
-// // // // // //             
-// // // // // //             xp   = data + (start1 - 1);
-// // // // // //             xend = data + (end1 - 1);
-// // // // // //             
-// // // // // //             minp = xp;
-// // // // // //             maxp = xp;
-// // // // // //             
-// // // // // //             while (xp < xend) {
-// // // // // //                 if (*xp > *maxp){
-// // // // // //                     maxp = xp;
-// // // // // //                 } else if (*xp < *minp) {
-// // // // // //                     minp = xp;
-// // // // // //                 }
-// // // // // //                 xp++; //Wrong for 2d
-// // // // // //             }
-// // // // // //             
-// // // // // //             all_max[0]   = *maxp;
-// // // // // //             all_min[0]   = *minp;
-// // // // // //             all_max_I[0] = (minp - data + 1); //Wrong for 2d
-// // // // // //             all_min_I[0] = (maxp - data + 1); //Wrong for 2d
-// // // // // //         }
-// // // // //         
-// // // // //     }
+        
+          /* Free allocated matrices */
+//     mxSetPr(x,xr);
+//     mxDestroyArray(x);
+//     mxDestroyArray(lhs[0]);
+//     mxDestroyArray(lhs[1]);
+    //plhs[0] = lhs[0];  
     
 }
+
+
+
+// // // // // //  int mexCallMATLAB(int nlhs, mxArray *plhs[], int nrhs, mxArray *prhs[], const char *functionName);
+// // // // // //                 
+// // // // // //                 
+// // // // // //                 
+// // // // // //                 minp = xp;
+// // // // // //                 maxp = xp;
+// // // // // //                 
+// // // // // //                 while (xp < xend) {
+// // // // // //                     if (*xp > *maxp){
+// // // // // //                         maxp = xp;
+// // // // // //                     } else if (*xp < *minp) {
+// // // // // //                         minp = xp;
+// // // // // //                     }
+// // // // // //                     xp++;
+// // // // // //                 }
+// // // // // //                 
+// // // // //                 
+// // // // //                 //all_max[assignment_offset]   = *maxp;
+// // // // //                 //all_min[assignment_offset]   = *minp;
+// // // // //                 //all_max_I[assignment_offset] = minp - data_offset + 1;
+// // // // //                 //all_min_I[assignment_offset] = maxp - data_offset + 1;
