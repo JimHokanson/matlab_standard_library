3f51c56e02a2c763c6d15e5ed3abf5481c9ac5f5
Jim Hokanson <jim.hokanson@gmail.com>
2015-01-13 21:17:36 -0500

    Added check for figure with no axes

--- +sl/+plot/+postp/linkFigureAxes.m
+++ +sl/+plot/+postp/linkFigureAxes.m
@@ -7,11 +7,17 @@ function linkFigureAxes(fig_handle,link_option)
 %   -------
 %   fig_handle :
 %   link_option : {'x','y','xy'}
-%       
+%
 %   TODO: We could expand link option to include features like linking rows
 %   and columns
 
 axes = findobj(fig_handle,'Type','axes');
+
+if isempty(axes)
+    error('No axes were found for the current figure handle')
+end
+
+%TODO: Have a check here
 
 %Stop incorrectly shrinking things ...
 %TODO: This could be its own function
@@ -19,37 +25,36 @@ xlim = [Inf -Inf];
 ylim = [Inf -Inf];
 
 for iAxes = 1:length(axes)
-   cur_axes = axes(iAxes);
-   cur_x = get(cur_axes,'xlim');
-   cur_y = get(cur_axes,'ylim');
-   if cur_x(1) < xlim(1)
-       xlim(1) = cur_x(1);
-   end
-   if cur_x(2) > xlim(2)
-       xlim(2) = cur_x(2);
-   end
-      if cur_y(1) < ylim(1)
-       ylim(1) = cur_y(1);
-   end
-   if cur_y(2) > ylim(2)
-       ylim(2) = cur_y(2);
-   end
+    cur_axes = axes(iAxes);
+    cur_x = get(cur_axes,'xlim');
+    cur_y = get(cur_axes,'ylim');
+    if cur_x(1) < xlim(1)
+        xlim(1) = cur_x(1);
+    end
+    if cur_x(2) > xlim(2)
+        xlim(2) = cur_x(2);
+    end
+    if cur_y(1) < ylim(1)
+        ylim(1) = cur_y(1);
+    end
+    if cur_y(2) > ylim(2)
+        ylim(2) = cur_y(2);
+    end
 end
 
 if any(strfind(link_option,'x'))
-for iAxes = 1:length(axes)
-   cur_axes = axes(iAxes);
-   set(cur_axes,'xlim',xlim);
-end
+    for iAxes = 1:length(axes)
+        cur_axes = axes(iAxes);
+        set(cur_axes,'xlim',xlim);
+    end
 end
 
 if any(strfind(link_option,'y'))
-for iAxes = 1:length(axes)
-   cur_axes = axes(iAxes);
-   set(cur_axes,'ylim',ylim);
+    for iAxes = 1:length(axes)
+        cur_axes = axes(iAxes);
+        set(cur_axes,'ylim',ylim);
+    end
 end
-end
 
-
-
+%Call to the actual linking code
 linkaxes(axes,link_option)
