9c22bd4dc182868fb265ebc51ef9b7cfbed497ec
Jim Hokanson <jim.hokanson@gmail.com>
2015-01-20 21:37:37 -0500

    sl.plot.postp.autoscale is rougly done, now it needs to be refactored

--- +sl/+plot/+postp/autoscale.m
+++ +sl/+plot/+postp/autoscale.m
@@ -7,6 +7,11 @@ function autoscale(h_axes)
 %   -----------
 %   1) Mean and Standard deviation
 %   2) CDF
+%
+%
+%   See Also:
+%   ---------
+%   sl.math.mergeVariances
 
 %CURRENT STATUS:
 %Line 306 in sl.plot.big_data.LinePlotReducer.renderData
@@ -53,10 +58,56 @@ else
     y_variance = sl.math.mergeVariances(y_vars,y_means,n_samples);
 end
 
-%TODO: Implement y std
-
 keyboard
 
+y_std = sqrt(y_variance);
+
+y_min = y_mean - 5*y_std;
+y_max = y_mean + 5*y_std;
 
 
-%Now determine an appropriate range
+%Let's consider setting y_min and y_max being equal
+%--------------------------------------------------
+y_range = y_max - y_min;
+
+dist_from_zero = abs(y_mean);
+
+pct_range = dist_from_zero/y_range;
+
+if pct_range < 0.05 %TODO: Move to variable
+   y_abs = max(abs(y_min),abs(y_max));
+   y_min = -y_abs;
+   y_max = y_abs;
+end
+
+%TODO: Let's ensure that we're within range
+%-------------------------------------------
+n_outside = 0;
+n_samples_total = sum(n_samples);
+for iC = 1:n_children
+   p = getappdata(c(iC),'BigDataPointer');
+   if isempty(p)
+      temp_y_data = get(c(iC),'YData');
+   else
+      temp_y_data = p.getYData; 
+   end
+   
+   %nnz - is this more memory friendly????
+   n_outside = n_outside + sum(temp_y_data > y_max);
+   n_outside = n_outside + sum(temp_y_data < y_min);
+   
+   n_samples(iC) = length(temp_y_data);
+   y_means(iC)   = mean(temp_y_data);
+   y_vars(iC)    = var(temp_y_data);
+end
+
+
+
+set(h_axes,'YLim',[y_min y_max])
+
+end
+
+function h__getYData(h_line)
+
+
+end
