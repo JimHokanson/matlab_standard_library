d7830481120a16ace42b0495b2de08bc3e9c6dd5
JimHokanson <jim.hokanson@gmail.com>
2015-03-05 17:47:37 -0500

    Working on more memory efficient filtering

--- +sci/+time_series/+filter/savitzky_golay.m
+++ +sci/+time_series/+filter/savitzky_golay.m
@@ -2,11 +2,24 @@ classdef savitzky_golay
     %
     %   Class:
     %   sci.time_series.filter.savitzky_golay
+    %
+    %   NOT YET IMPLEMENTED
+    %
+    %   See Also:
+    %   sgolayfilt
     
     properties
     end
     
     methods
+        function obj = savitzky_golay()
+            
+            %sgolayfilt(X,K,F,W)
+           %K - order, must be less than F 
+           %    polynomial order to fit to the data (locally)
+           %F - frame size, F, F must be odd
+           %    I think this is in samples
+        end
     end
     
 end
