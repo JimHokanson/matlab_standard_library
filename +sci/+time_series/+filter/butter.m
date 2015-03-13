b70d9fbaf74fb901fb1b40505eeaaef385e7c360
Jim Hokanson <jim.hokanson@gmail.com>
2015-03-12 20:32:04 -0400

    Add plotting to butter

--- +sci/+time_series/+filter/butter.m
+++ +sci/+time_series/+filter/butter.m
@@ -2,8 +2,8 @@ classdef butter < handle
     %
     %   Class:
     %   sci.time_series.filter.butter
-    %   
-    %   A Butterworth filter is maximially flat in the pass band with the 
+    %
+    %   A Butterworth filter is maximially flat in the pass band with the
     %   trade off of not being very steep in transition, thus typically
     %   requiring a higher order filter.
     %
@@ -17,11 +17,11 @@ classdef butter < handle
     %   sci.time_series.filter.smoothing
     
     properties
-       %-----------------------
-       order   %Filter order
-       cutoff_or_cutoffs
-       type
-       zero_phase %logical
+        %-----------------------
+        order   %Filter order
+        cutoff_or_cutoffs
+        type
+        zero_phase %logical
     end
     
     methods
@@ -31,13 +31,13 @@ classdef butter < handle
             %   obj = butter(order,cutoff_or_cutoffs,type,varargin)
             %
             %   Inputs:
-            %   ------- 
+            %   -------
             %   order : number
             %       The order of the filter. Higher orders lead to ... (JAH
             %       TODO: finish description)
-            %   
-            %   cutoff_or_cutoffs : 
-            %       The frequency bands to cutoff at, in Hz. 
+            %
+            %   cutoff_or_cutoffs :
+            %       The frequency bands to cutoff at, in Hz.
             %
             %   type : {'low','high','stop','pass'}
             %
@@ -55,51 +55,75 @@ classdef butter < handle
             %   f = sci.time_series.filter.butter(2,[55 65],'stop');
             %
             
-           in.zero_phase = true;
-           in = sl.in.processVarargin(in,varargin);
-           
-           obj.order  = order;
-           
-           if strcmp(type,'pass') || strcmp(type,'band')
-               type = 'bandpass';
-           end
-           
-           obj.type   = type;
-           obj.zero_phase = in.zero_phase;
-           obj.cutoff_or_cutoffs = cutoff_or_cutoffs;
+            in.zero_phase = true;
+            in = sl.in.processVarargin(in,varargin);
+            
+            obj.order  = order;
+            
+            if strcmp(type,'pass') || strcmp(type,'band')
+                type = 'bandpass';
+            end
+            
+            obj.type   = type;
+            obj.zero_phase = in.zero_phase;
+            obj.cutoff_or_cutoffs = cutoff_or_cutoffs;
         end
-        function data_out = filter(obj,data_in,fs)
-           %x Filter the data
-           %
-           %    Inputs:
-           %    -------
-           %    fs : scalar
-           %        Sampling rate
-           %
+        function data = filter(obj,data,fs)
+            %x Filter the data
+            %
+            %    Inputs:
+            %    -------
+            %    fs : scalar
+            %        Sampling rate
+            %
             
-           %TODO: Check cutoffs vs fs ... 
+            %TODO: Check cutoffs vs fs ...
             
-           [B,A] = obj.getCoefficients(fs);
-           
-           if obj.zero_phase
-               filter_method = @filtfilt;
-           else
-               filter_method = @filter;
-           end
-           
-           data_out = filter_method(B,A,data_in);
+            [B,A] = obj.getCoefficients(fs);
+            
+            if obj.zero_phase
+                filter_method = @filtfilt;
+            else
+                filter_method = @filter;
+            end
+            
+            data = filter_method(B,A,data);
         end
         function [B,A] = getCoefficients(obj,fs)
-           %x Compute the coefficients
-           %
-           %    Inputs:
-           %    -------
-           %    fs : scalar
-           %        Sampling rate
-           %
-           [B,A] = butter(obj.order,obj.cutoff_or_cutoffs/(fs/2),obj.type); 
+            %x Compute the coefficients
+            %
+            %    Inputs:
+            %    -------
+            %    fs : scalar
+            %        Sampling rate
+            %
+            [B,A] = butter(obj.order,obj.cutoff_or_cutoffs/(fs/2),obj.type);
+        end
+        function plotFrequencyResponse(obj,fs,varargin)
+            %
+            %
+            %  plotFrequencyResponse(obj,fs,varargin)
+            %
+            %  Optional Inputs:
+            %  ----------------
+            %
+            %  See Also:
+            %  freqz
+            
+            in.N = 1024;
+            in = sl.in.processVarargin(in,varargin);
+            
+            [B,A] = getCoefficients(obj,fs);
+            
+            [H,F] = freqz(B,A,in.N,fs);
+            
+            plot(F,abs(H))
         end
         function str = getSummaryString(obj,fs)
+            %
+            %   str = getSummaryString(obj,fs)
+            %   
+            
             if obj.zero_phase
                 filter_method = 'filtfilt()';
             else
@@ -122,13 +146,13 @@ classdef butter < handle
                     freq_str = sprintf('from %g to %g',freq_values(1),freq_values(2));
             end
             
-           str = sprintf('Butterworth Filter: type: %s,  frequency: %s,  order: %d,  method: %s',filter_type,freq_str,obj.order,filter_method);
+            str = sprintf('Butterworth Filter: type: %s,  frequency: %s,  order: %d,  method: %s',filter_type,freq_str,obj.order,filter_method);
         end
         function disp(obj)
-%            fprintf('xxxxxxx\n')
-%            fprintf(inputname(1))
-%            fprintf('xxxxxxx\n') 
-           sl.obj.dispObject_v1(obj) 
+            %            fprintf('xxxxxxx\n')
+            %            fprintf(inputname(1))
+            %            fprintf('xxxxxxx\n')
+            sl.obj.dispObject_v1(obj)
         end
     end
 end
