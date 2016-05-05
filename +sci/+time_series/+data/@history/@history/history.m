399c0d7fb166439a3f8c594083af40105f19300f
JimHokanson <jim.hokanson@gmail.com>
2016-05-05 10:23:07 -0400

    Starting work on history class to better track changes to class

---
+++ +sci/+time_series/+data/@history/@history/history.m
@@ -0,0 +1,21 @@
+classdef history < handle
+    %
+    %   Class:
+    %   sci.time_series.data.history
+    
+    properties
+        sources
+        messages
+    end
+    
+    methods
+        function addElements(obj)
+           in.direct = true;
+           in = sl.in.processVarargin(in,varargin);
+           
+           
+        end
+    end
+    
+end
+
