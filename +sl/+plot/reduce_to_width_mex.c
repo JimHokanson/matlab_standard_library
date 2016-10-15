3a9e0697009dc60e451b73a105c063a8ded60b56
Jim Hokanson <jim.hokanson@gmail.com>
2016-10-15 17:27:17 -0400

    updated reduce_to_width code to support chunks, still needs starts and stops, as well as finishing of incomplete chunks and possible openmp collapse and explicit simd usage

--- +sl/+plot/reduce_to_width_mex.c
+++ +sl/+plot/reduce_to_width_mex.c
@@ -6,17 +6,26 @@
 //
 
 /*
+n_channels = 2;
 data = [1 2 3 4 7    1 8 1 8 9   2 9 8 3 9    2 4 5 6 2    9 3 4 8 9    3 9 4 2 3   4 9 0 2 9]';
-%data = repmat(data,[1 4]);
-data = repmat(data,[5000000 4]);
-data(23,1:4) = [-1 -2 -3 -4];
+%data = repmat(data,[1 n_channels]);
+data = repmat(data,[5000000 n_channels]);
+data(23,1:n_channels) = -1:-1:-1*n_channels;
 len_data_p1 = size(data,1)+1;
-bounds = [1:5:len_data_p1];
  tic
  for i = 1:10
-[min_data,max_data] = sl.plot.reduce_to_width_mex(bounds,data);
+[min_data,max_data] = sl.plot.reduce_to_width_mex([],data);
  end
  toc/10
+ 
+ tic
+ for i = 1:10
+ min_data2 = min(reshape(data,[5 35000000 n_channels]),[],1);
+ max_data = max(reshape(data,[5 35000000 n_channels]),[],1);
+ end
+ toc/10
+ 
+ isequal(min_data,squeeze(min_data2))
 */
 
 void mexFunction(int nlhs, mxArray *plhs[], int nrhs, const mxArray*prhs[])
@@ -31,78 +40,47 @@ void mexFunction(int nlhs, mxArray *plhs[], int nrhs, const mxArray*prhs[])
     //Let's assume we are given the indices
     //Later on we can add that code here as well ...
     
-    double min;
-    double max;
-    //double *current_data_point;
-    double *bound_indices;
-    double *data;
-    double *min_data;
-    double *max_data;
-    double *p_min_data;
-    double *p_max_data;
-    mwSize n_samples, n_chans;
-    mwSize n_bounds;
-    mwSize start_sample;
-    mwSize stop_sample;
     
-    bound_indices = mxGetData(prhs[0]);
-    n_bounds = mxGetNumberOfElements(prhs[0]);
+    double *input_data = mxGetData(prhs[1]);
+    mwSize n_samples = mxGetM(prhs[1]);
+    mwSize n_chans = mxGetN(prhs[1]);
     
-    data = mxGetData(prhs[1]);
-    n_samples = mxGetM(prhs[1]);
-    n_chans = mxGetN(prhs[1]);
+    //TODO: Get this from the user ...
+    mwSize samples_per_chunk = 5;
     
-    min_data = mxMalloc(8*n_chans*(n_bounds-1));
-    max_data = mxMalloc(8*n_chans*(n_bounds-1));
+    //TODO: Also need start and end ...
     
-//     mwSize data_chan_offset = 0;
-//     mwSize bounds_chan_offset = 0;
-//     p_min_data = min_data;
-//     p_max_data = max_data;
-//     //#pragma omp parallel for
-//     for (mwSize iChan = 0; iChan < n_chans; iChan++){
-//         //#pragma omp parallel for
-//         //- 1 for 0-based and -1 for going 1 before that
-//         stop_sample = ((mwSize) bound_indices[0]-1) + data_chan_offset - 1;
-//         for (mwSize iBound = 0; iBound < (n_bounds-1); iBound++){
-//             //TODO: Need an offset in here for the channel ...
-//             start_sample = stop_sample + 1;
-//             //start_sample = ((mwSize) bound_indices[iBound]-1) + data_chan_offset;
-//             stop_sample = ((mwSize) bound_indices[iBound+1]-1) + data_chan_offset;
-//             //mexPrintf("Start and stop: %d  %d\n",start_sample,stop_sample);
-//             current_data_point = &data[start_sample];
-//             min = max = *current_data_point;
-//             for (mwSize j = start_sample + 1; j < stop_sample; j++){
-//                 if (*(++current_data_point) > max){
-//                     max = *current_data_point;
-//                 }else if (*current_data_point < min){
-//                     min = *current_data_point;
-//                 }
-//             }
-//             *(++p_min_data) = min;
-//             *(++p_max_data) = max;
-//         }
-//         
-//         data_chan_offset = n_samples*(iChan + 1);
-// //         bounds_chan_offset = (n_bounds-1)*(iChan);
-//         //mexPrintf("chan offset: %d\n",chan_offset);
-//     }
+    //Integer division, should floor as desired
+    mwSize n_chunks = n_samples/samples_per_chunk;
     
-    #pragma omp parallel for
+    double *min_data = mxMalloc(8*n_chans*n_chunks);
+    double *max_data = mxMalloc(8*n_chans*n_chunks);
+    
+    #pragma omp parallel for simd
     for (mwSize iChan = 0; iChan < n_chans; iChan++){
-        mwSize data_chan_offset = n_samples*iChan;
-        double *current_data_point;
-        current_data_point = data + data_chan_offset;
-        min = max = *current_data_point;
-        for (mwSize iSample = 1; iSample < n_samples; iSample++){
+        double *current_data_point = input_data + n_samples*iChan;
+        double *local_min_data = min_data + n_chunks*iChan - 1;
+        double *local_max_data = max_data + n_chunks*iChan - 1;
+        double min;
+        double max;
+        for (mwSize iChunk = 0; iChunk < n_chunks; iChunk++){
+            min = *current_data_point;
+            max = *current_data_point;
+            
+            for (mwSize iSample = 1; iSample < samples_per_chunk; iSample++){
                 if (*(++current_data_point) > max){
                     max = *current_data_point;
                 }else if (*current_data_point < min){
                     min = *current_data_point;
                 }
+            }
+            ++current_data_point;
+            *(++local_min_data) = min;
+            *(++local_max_data) = max;
         }
-        min_data[iChan] = min;
-        max_data[iChan] = max;
+        
+        //TODO: We might have one trailing bit of data to handle that didn't
+        //fit evenly into the search ...
     }
     
     
@@ -115,9 +93,9 @@ void mexFunction(int nlhs, mxArray *plhs[], int nrhs, const mxArray*prhs[])
     
     mxSetData(plhs[0],min_data);
     mxSetData(plhs[1],max_data);
-    mxSetM(plhs[0],n_bounds-1);
+    mxSetM(plhs[0],n_chunks);
     mxSetN(plhs[0],n_chans);
-    mxSetM(plhs[1],n_bounds-1);
+    mxSetM(plhs[1],n_chunks);
     mxSetN(plhs[1],n_chans);
     
     
