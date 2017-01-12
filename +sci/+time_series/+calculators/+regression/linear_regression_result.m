44639b27df861914c00735845a1a803a942d6e7c
JimHokanson <jim.hokanson@gmail.com>
2017-01-12 16:48:56 -0500

    added files for regression and the result

---
+++ +sci/+time_series/+calculators/+regression/linear_regression_result.m
@@ -0,0 +1,12 @@
+classdef linear_regression_result
+    %UNTITLED6 Summary of this class goes here
+    %   Detailed explanation goes here
+    
+    properties
+    end
+    
+    methods
+    end
+    
+end
+
---
+++ +sci/+time_series/+calculators/@regression/regression.m
@@ -0,0 +1,12 @@
+classdef regression
+    %UNTITLED5 Summary of this class goes here
+    %   Detailed explanation goes here
+    
+    properties
+    end
+    
+    methods
+    end
+    
+end
+
