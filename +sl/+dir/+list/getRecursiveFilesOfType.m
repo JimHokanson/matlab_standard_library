ba50e56b5f7ed6aeac67442e6bff6a2d6fc6fc0b
JimHokanson <jim.hokanson@gmail.com>
2018-08-21 10:41:34 -0400

    Changed default output type to paths
    Previous default of names didn't make much sense for a recursive search where the directory name would change

--- +sl/+dir/+list/getRecursiveFilesOfType.m
+++ +sl/+dir/+list/getRecursiveFilesOfType.m
@@ -9,14 +9,14 @@ function output = getRecursiveFilesOfType(start_path,file_extension,varargin)
 %
 %   Optional Inputs
 %   ---------------
-%   output_type : {'object','names','paths','dir'} (default 'object')
+%   output_type : {'object','names','paths','dir'} (default 'paths')
 %       - object : return sl.dir.list_result
 %       - names  : return only the names
 %       - paths   : return only the paths
 %       - dir    : return only the dir structures
 
 in.waitbar_string = '';
-in.output_type = 'names';
+in.output_type = 'paths';
 in = sl.in.processVarargin(in,varargin);
 
 output = sl.dir.getList(start_path,'output_type',in.output_type,...
