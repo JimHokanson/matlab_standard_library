c7171709977e6deb0fa389bb5aa503110426910b
JimHokanson <jim.hokanson@gmail.com>
2020-03-16 14:41:53 -0400

    Added extra conversion option

--- +sci/+units/getConversionFunction.m
+++ +sci/+units/getConversionFunction.m
@@ -31,7 +31,7 @@ switch combined_units
         fh = @(x)times(x,1e6);
     case 'V#mV'
         fh = @(x)times(x,1e3);
-    case {'cmH20#cm H20','cm H20#cm H2O','cmH20#cm H2O'}
+    case {'cmH20#cm H20','cm H20#cm H2O','cmH20#cm H2O','#cm H2O'}
         %Yikes, why aren't we using the letter O instead of the # 0 ...
         fh = @(x)x;
 % % %     case 's#ms'
