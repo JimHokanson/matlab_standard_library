fd067e0c782af2bd2c5bbc18e6bb3bd9330e9075
Jim Hokanson <jim.hokanson@gmail.com>
2020-06-02 14:18:59 -0400

    Update getPosition.m

--- +sl/+hg/+axes/getPosition.m
+++ +sl/+hg/+axes/getPosition.m
@@ -1,19 +1,32 @@
 function p = getPosition(h_axes,varargin)
+%x retrieves position information for axes handles
 %
 %   p = sl.hg.axes.getPosition(h_axes,varargin)
 %
+%   This function exists for a couple of reasons:
+%   1) To return position information as a struct rather than an array. The
+%   default position is an array where you need to remember what each
+%   element corresponds to, rather than labeled properties (e.g. left,
+%   bottom)
+%   2) To take into account a colorbar or legend as part of the position.
+%
+%   https://www.mathworks.com/help/matlab/creating_plots/automatic-axes-resize.html
+%   
 %   Inputs
 %   ------
-%   h_axes : [1 x n] 
+%   h_axes : [1 n] or {1 n}
 %
 %   Optional Inputs
 %   ---------------
-%   add_legend : default false
-%   add_color : default false
+%   add_legend : default false NYI
+%       If true the extents of the axes are extended to include the legend
+%   add_colorbar : default false
+%       If true the extents of the axes are extended to include the
+%       colorbar
 %   type : default 'tight'
-%       'tight' 't'
-%       'outer' 'o'
-%       'inner' 'p'
+%       'tight' 't' - axes + text
+%       'outer' 'o' - axes + text + ?? margins
+%       'inner' 'p' - axes only
 %   as_struct : default false
 %       TODO: I think true would be preferable, where is this being used in
 %       the code base????
@@ -24,6 +37,15 @@ function p = getPosition(h_axes,varargin)
 %       When 2 or more axes are given, the result is a cell or struct
 %       array. Toggling between array/cell and struct/struct_array is done
 %       by the 'as_struct' option.
+%
+%       1 axes
+%           array - as_struct=false
+%           struct - as_struct=true
+%       2+ axes
+%           {array} - as_struct=false
+%           struct_array - as_struct=true
+%           struct - as_struct=true
+%
 %   
 %       Note, the struct has the following format:
 %       .left
@@ -32,32 +54,50 @@ function p = getPosition(h_axes,varargin)
 %       .height
 %       .top
 %       .right
+%
+%   Examples
+%   --------
+%   sp = 
+%
+%
+%   See Also
+%   --------
+%   sl.hg.figure.getPosition
+%   sl.plot.subplotter
+%   sl.hg.axes.drawPositionBox
 
 %TODO: We need to respect different units
 
 in.flatten_struct = false;
-in.as_struct = false;
+in.as_struct = true;
 in.add_legend = false;
 in.add_colorbar = false;
 in.type = 'tight';
 in = sl.in.processVarargin(in,varargin);
 
+if in.flatten_struct
+    in.as_struct = true;
+end
+
 if length(h_axes) == 1
     p = h__getPosition(h_axes,in);
+elseif iscell(h_axes)
+    p = arrayfun(@(x) h__getPosition(x,in),h_axes,'un',0);
 else
     p = arrayfun(@(x) h__getPosition(x,in),h_axes,'un',0);
-    if in.as_struct
-       p = [p{:}]; 
-    end
-    if in.flatten_struct
-        s = struct('left',[p.left],...
-            'bottom',[p.bottom],...
-            'width',[p.width],...
-            'height',[p.height],...
-            'right',[p.right],...
-            'top',[p.top]);  
-        p = s;
-    end
+end
+
+if in.as_struct && iscell(p)
+   p = [p{:}]; 
+end
+if in.flatten_struct
+    s = struct('left',[p.left],...
+        'bottom',[p.bottom],...
+        'width',[p.width],...
+        'height',[p.height],...
+        'right',[p.right],...
+        'top',[p.top]);  
+    p = s;
 end
 
 end
@@ -102,6 +142,7 @@ end
 
 if in.as_struct
     %The -1 assumes pixel position
+    %TODO: Remove this assumption ...
     s = struct('left',p(1),'bottom',p(2),'width',p(3),'height',p(4),...
         'right',p(1)+p(3)-1,'top',p(2)+p(4)-1);
     p = s;
