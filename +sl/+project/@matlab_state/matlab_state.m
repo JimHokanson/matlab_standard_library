24025c940077a913fa8dcddd2714f05abb3d32d9
Jim Hokanson <jim.hokanson@gmail.com>
2020-12-01 10:45:34 -0500

    Create matlab_state.m
    added skeleton, NYI

---
+++ +sl/+project/@matlab_state/matlab_state.m
@@ -0,0 +1,15 @@
+classdef matlab_state
+    %
+    %   Class:
+    %   sl.project.matlab_state
+    
+    properties
+        Property1
+    end
+    
+    methods
+        function obj = matlab_state()
+        end
+    end
+end
+
