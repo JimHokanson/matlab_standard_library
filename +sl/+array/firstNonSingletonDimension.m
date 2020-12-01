099b53de4c0102d5b2080c6b951b7e3c2c19519a
Jim Hokanson <jim.hokanson@gmail.com>
2020-12-01 10:45:51 -0500

    Update firstNonSingletonDimension.m
    Bug fix ...

--- +sl/+array/firstNonSingletonDimension.m
+++ +sl/+array/firstNonSingletonDimension.m
@@ -4,7 +4,7 @@ function dim = firstNonSingletonDimension(data)
 %
 %   TODO: Finish documentation ...
 
-dim = find(size(data) > 1);
+dim = find(size(data) > 1,1);
 if isempty(dim)
     dim = 1;
 end
