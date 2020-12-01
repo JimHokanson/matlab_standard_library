dd2b153111e9779cc40fc4fd92476847b5a35f8f
Jim Hokanson <jim.hokanson@gmail.com>
2020-12-01 10:45:20 -0500

    Update setField.m
    documentation update

--- +sl/+struct/setField.m
+++ +sl/+struct/setField.m
@@ -1,3 +1,32 @@
 function setField
+%
+%   Code relies on mex, compile with:
+%   mex setField.c
+%
+%   Examples
+%   ---------
+%   s = struct;
+%   wtf = sl.struct.setField(s,'wtf batman',5);
+%
+%   %Note, access can be done with:
+%   wtf.('wtf batman')
+%
+%   %Override test
+%   wtf = sl.struct.setField(wtf,'wtf batman',1);
+%
+%   %More testing
+%   wtf.test = 3;
+%   wtf.nope = 4;
+%   wtf = sl.struct.setField(wtf,'nope','cheese');
+%   wtf = sl.struct.setField(wtf,'! !','wow');
+%   wtf = sl.struct.setField(wtf,'test',struct());
+%
+%   wtf = 
+%   struct with fields:
+% 
+%     wtf batman: 1
+%           test: [1×1 struct]
+%           nope: 'cheese'
+%            ! !: 'wow'
 
 error('Mex function setField.c not compiled')
