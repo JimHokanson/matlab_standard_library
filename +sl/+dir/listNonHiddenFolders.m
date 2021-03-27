205318772bdf78f42299a6bc4760123be9416f3d
Jim Hokanson <jim.hokanson@gmail.com>
2021-03-27 11:06:22 -0500

    Update listNonHiddenFolders.m
    added full paths option

--- +sl/+dir/listNonHiddenFolders.m
+++ +sl/+dir/listNonHiddenFolders.m
@@ -1,23 +1,29 @@
-function dir_names = listNonHiddenFolders(rootPath,varargin)
+function dir_names = listNonHiddenFolders(root_path,varargin)
 %listNonHiddenFolders  Lists not hidden folders and omits .. & .
 %
 %   This function returns subfolders which are not hidden. It also omits
 %   the . and .. directories.
 %
-%   dir_names = sl.dir.listNonHiddenFolders(rootPath)
+%   folder_names = sl.dir.listNonHiddenFolders(root_path)
 %
-%   TODO: document optional inputs
+%   folders_and_files = sl.dir.listNonHiddenFolders(root_path,'keep_files',true)
 %
-%   TODO: This only respects the hidden attribute and not whether or
-%   not we are showing hidden files
-%   
-%   See Also:
+%   paths = sl.dir.listNonHiddenFolders(root_path,'full_path',true)
+%
+%   Optional Inputs
+%   ---------------
+%   full_path : default false
+%   keep_files : default false
+%
+%   See Also
+%   --------
 %   fileattrib
 
+in.full_path = false;
 in.keep_files = false;
 in = sl.in.processVarargin(in,varargin);
 
-d         = dir(rootPath);
+d = dir(root_path);
 if in.keep_files
     keep_mask = true(1,length(d));
 else
@@ -31,7 +37,7 @@ if ispc
     keep_mask(strcmp(dir_names,'.'))  = false;
     keep_mask(strcmp(dir_names,'..')) = false;
     for I = find(keep_mask)
-       [~,stats]    = fileattrib(fullfile(rootPath,dir_names{I}));
+       [~,stats] = fileattrib(fullfile(root_path,dir_names{I}));
        keep_mask(I) = ~stats.hidden;
     end
 elseif ismac
@@ -43,6 +49,9 @@ else
 end
 
 dir_names = dir_names(keep_mask);
+if in.full_path
+   dir_names = sl.dir.fullfileCA(root_path,dir_names); 
+end
 
 
 end
