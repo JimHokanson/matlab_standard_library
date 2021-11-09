680e196b9c39c06261836f65fa78d3e318b104b8
Jim Hokanson <jim.hokanson@gmail.com>
2021-11-08 21:35:45 -0600

    Create mergePDFs.m

---
+++ +sl/+io/mergePDFs.m
@@ -0,0 +1,25 @@
+function mergePDFs(input_file_paths,output_file_path)
+%
+%   sl.io.mergePDFs(input_file_paths,output_file_path)
+%
+%   Based on:
+%   https://www.mathworks.com/matlabcentral/fileexchange/89127-merge-pdf-documents
+
+%https://pdfbox.apache.org/docs/2.0.5/javadocs/org/apache/pdfbox/io/MemoryUsageSetting.html#setupMainMemoryOnly()
+memory_settings = org.apache.pdfbox.io.MemoryUsageSetting.setupMainMemoryOnly();
+
+%https://pdfbox.apache.org/docs/2.0.1/javadocs/org/apache/pdfbox/multipdf/PDFMergerUtility.html
+merger = org.apache.pdfbox.multipdf.PDFMergerUtility;
+
+for i = 1:length(input_file_paths)
+    if ~exist(input_file_paths{i},'file')
+        error('File does not exist: %s',input_file_paths{i})
+    end
+    merger.addSource(input_file_paths{i});
+end
+
+merger.setDestinationFileName(output_file_path)
+
+merger.mergeDocuments(memory_settings)
+
+end
