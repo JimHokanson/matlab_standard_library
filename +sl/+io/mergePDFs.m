e3e1ef002690757ae87a175c01ae8b9a49c05506
Jim Hokanson <jim.hokanson@gmail.com>
2021-11-22 09:48:57 -0600

    updating sl.io.MergePDFs to optionally delete inputs

--- +sl/+io/mergePDFs.m
+++ +sl/+io/mergePDFs.m
@@ -1,9 +1,13 @@
-function mergePDFs(input_file_paths,output_file_path)
+function mergePDFs(input_file_paths,output_file_path,varargin)
 %
 %   sl.io.mergePDFs(input_file_paths,output_file_path)
 %
 %   Based on:
 %   https://www.mathworks.com/matlabcentral/fileexchange/89127-merge-pdf-documents
+
+
+in.delete_inputs = false;
+in = sl.in.processVarargin(in,varargin);
 
 %https://pdfbox.apache.org/docs/2.0.5/javadocs/org/apache/pdfbox/io/MemoryUsageSetting.html#setupMainMemoryOnly()
 memory_settings = org.apache.pdfbox.io.MemoryUsageSetting.setupMainMemoryOnly();
@@ -21,5 +25,11 @@ end
 merger.setDestinationFileName(output_file_path)
 
 merger.mergeDocuments(memory_settings)
+
+if in.delete_inputs
+    for i = 1:length(input_file_paths)
+        delete(input_file_paths{i});
+    end
+end
 
 end
