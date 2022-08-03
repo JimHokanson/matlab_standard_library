25f502136d87f91be9fef6a4e9a7106bfd5bbb5a
Auster-hw <whe@mcw.edu>
2022-08-03 15:15:11 -0500

    Update toComboMatrix.m

--- +sl/+table/toComboMatrix.m
+++ +sl/+table/toComboMatrix.m
@@ -12,7 +12,7 @@ function [output,s] = toComboMatrix(data,target,dimension_fields,varargin)
 %   ---------------
 %   merge_function : function_handle
 %   dims : struct
-%   
+%
 %
 %   Output
 %   ------
@@ -42,16 +42,16 @@ function [output,s] = toComboMatrix(data,target,dimension_fields,varargin)
 %   output = sl.table.toComboMatrix(data,'C',{'A','B'},'merge_function','keep_first');
 %   output = sl.table.toComboMatrix(data,'C',{'A','B'},'merge_function',@max);
 %
-%        B  2 3 
+%        B  2 3
 %   A    ---
 %  0.5   3 4
-%   1    8 5 
+%   1    8 5
 %
 %   s = struct;
 %   s.A = [1,0.5];
 %   output = sl.table.toComboMatrix(data,'C',{'A','B'},'merge_function',@max,'dims',s);
 %
-%     B  2 3 
+%     B  2 3
 %   A    ---
 %   1    8 5   %Note we've forced order of A to be 1 then 0.5, not sorted
 %  0.5   3 4
@@ -59,13 +59,13 @@ function [output,s] = toComboMatrix(data,target,dimension_fields,varargin)
 %   s = struct;
 %   s.A = [1];
 %   output = sl.table.toComboMatrix(data,'C',{'A','B'},'merge_function',@max,'dims',s);
-%   
-%     B  2 3 
+%
+%     B  2 3
 %   A    ---
 %   1    8 5   %Note we've ignored A=0.5
 %
 %
-%   
+%
 %   bc_mat = sl.table.toComboMatrix(data,'bladder_capacity',{'set','fill_rate','expt_id'});
 
 %   see IC from [U,IA,IC] = unique() - use IC to get indices from unique
@@ -75,24 +75,67 @@ function [output,s] = toComboMatrix(data,target,dimension_fields,varargin)
 %
 %   fieldnames() - name of fields in structure
 %   default value
+ norm_rates = [1/6 1/3 1 2 4 8 16];
 
-in.merge_function = @mean;
-in.default_value = NaN;
-in.dims = struct(); %empty, no fields overridden
-in = sl.in.processVarargin(in,varargin);
+% target = 'bladder_capacity';
+% dimension_fields = {'set','fill_rate','expt_id','base_fill_rate'};
+new_data = data(:, [sprintf(target) , dimension_fields]);
+new_data= new_data(new_data.set~=0,:);
+s= struct;
+G = findgroups(new_data.expt_id);
+output= NaN(6,length(norm_rates),max(G));
+        for i =1 : max(G)
+            indices = G==i;
+           temp = new_data(indices,:);
+        
+           for j = 1: length(unique(temp.set))
+        single_set= temp(temp.set==j,:);
+         base_rate = unique(single_set.base_fill_rate);
+        norm_rate_in_set = single_set.fill_rate ./ base_rate;
+        position_indices = dba.files.getPosition(norm_rate_in_set,norm_rates);
+       output (j,position_indices,i) = single_set.(sprintf(target));
+           end
 
-if ischar(in.merge_function)
-%    if strcmp(in.merge_function,'keep_first')
-%        in.merge_function = @keep_first;
-%    else
-in.merge_function = str2func(in.merge_function);
-%    end    
+        end
+  
+
+
+
+
+
+
+
+
+
+
+
 end
 
-keyboard
 
-end
 
-function output = keep_first(array)
-    output = array(1);
-end
+
+
+
+
+% 
+% 
+% in.merge_function = @mean;
+% in.default_value = NaN;
+% in.dims = struct(); %empty, no fields overridden
+% in = sl.in.processVarargin(in,varargin);
+% 
+% if ischar(in.merge_function)
+%     %    if strcmp(in.merge_function,'keep_first')
+%     %        in.merge_function = @keep_first;
+%     %    else
+%     in.merge_function = str2func(in.merge_function);
+%     %    end
+% end
+% 
+% keyboard
+% 
+% end
+% 
+% function output = keep_first(array)
+% output = array(1);
+% end
