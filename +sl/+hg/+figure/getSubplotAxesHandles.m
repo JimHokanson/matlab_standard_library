bd1a7a368d21d9cf1e0297c3b28aa0f23726b83c
Jim Hokanson <jim.hokanson@gmail.com>
2022-11-20 17:38:45 -0600

    cleaning up how we get subplot handles after the fact for subplotter

--- +sl/+hg/+figure/getSubplotAxesHandles.m
+++ +sl/+hg/+figure/getSubplotAxesHandles.m
@@ -7,7 +7,7 @@ function s = getSubplotAxesHandles(figure_handle)
 %   s : struct - ideally this would be a class
 %                   sl.figure.results.subplot_handles_info
 %   .grid_handles :
-%       A matrix of handles. How are empties handled? 
+%       A matrix of handles. How are empties handled?
 %              empty => GraphicsPlaceholder with no properties.
 %       Shape represents the subplot shape.
 %   .is_valid : logical mask
@@ -41,64 +41,76 @@ s = sl.hg.figure.getSubplotAxesHandles(gcf)
 
 
 %}
-  
+
 
 %TODO: It looks like this doesn't return things in grid order ...
 %wtf matlab ;asdfalsdfklaslkdfla;sdlkfals;dflk;
 
-    s = struct;
-    
-    %This should work in 2014b, not sure if it works earlier ...
-    %Works in 2014a
-    %There is a bug in 2015a
-    
-    %This also fails after any manipulations ...
-    %I found this to be wrong in some cases
-    %s.grid_handles = flipud(getappdata(figure_handle, 'SubplotGrid'));
+s = struct;
 
-    %THIS NEEDS TO BE FIXED - NOT ROBUST TO MINOR DIFFERENCES IN GRID
-    %LOCATIONS
-    
-    % - close should be determined by the magnitude of spacing between
-    %   axes
-    % - TODO: We can also try assuming a full grid and factor
-    %       e.g. 8 should be either 2 x 4 or 4 x 2
-    %       - this however assumes no extra axes ...
-    
-    %if isempty(s.grid_handles)
-    
-    %TODO: This will fail if we have extra axes ...
-    %Can we make it more robust????
-    
-    handles=findobj(figure_handle,'Type','axes','Visible','on');
-    
-    positions = vertcat(handles.Position);
-    
-    %left bottom heigh width
-    
-    TOLERANCE = 0.0001;
-    
-    unique_col_positions = uniquetol(positions(:,1),TOLERANCE); %lefts
-    unique_row_positions = uniquetol(positions(:,2),TOLERANCE); %bottoms
-    
-    n_rows = length(unique_row_positions);
-    n_cols = length(unique_col_positions);
-    
-    [~,r_I] = ismembertol(positions(:,2),unique_row_positions,TOLERANCE);
-    [~,c_I] = ismembertol(positions(:,1),unique_col_positions,TOLERANCE);
-    
-    temp = cell(n_rows,n_cols);
-    
-    for i = 1:length(handles)
-        cur_h = handles(i);
-%         r_I = find(positions(i,2) == unique_row_positions);
-%         c_I = find(positions(i,1) == unique_col_positions);
-        temp{r_I(i),c_I(i)} = cur_h;
-    end
-    
-    temp1 = [temp{:}];
-    temp2 = reshape(temp1,n_rows,n_cols);
-    
+%This should work in 2014b, not sure if it works earlier ...
+%Works in 2014a
+%There is a bug in 2015a
+
+%This also fails after any manipulations ...
+%I found this to be wrong in some cases
+%s.grid_handles = flipud(getappdata(figure_handle, 'SubplotGrid'));
+
+% - close should be determined by the magnitude of spacing between
+%   axes
+% - TODO: We can also try assuming a full grid and factor
+%       e.g. 8 should be either 2 x 4 or 4 x 2
+%       - this however assumes no extra axes ...
+
+%if isempty(s.grid_handles)
+
+%TODO: This will fail if we have extra axes ...
+%Can we make it more robust????
+
+handles=findobj(figure_handle,'Type','axes','Visible','on');
+
+positions = vertcat(handles.Position);
+
+%left bottom height width
+
+%The idea here is that if we fail with a tight tolerance, we 
+
+is_good = false;
+for tolerance = [0.0001 0.05 0.1 0.2]
+    try
+        
+        unique_col_positions = uniquetol(positions(:,1),tolerance); %lefts
+        unique_row_positions = uniquetol(positions(:,2),tolerance); %bottoms
+        
+        n_rows = length(unique_row_positions);
+        n_cols = length(unique_col_positions);
+        
+        [~,r_I] = ismembertol(positions(:,2),unique_row_positions,tolerance);
+        [~,c_I] = ismembertol(positions(:,1),unique_col_positions,tolerance);
+        
+        temp = cell(n_rows,n_cols);
+        
+        for i = 1:length(handles)
+            cur_h = handles(i);
+            temp{r_I(i),c_I(i)} = cur_h;
+        end
+        
+        temp1 = [temp{:}];
+        %This is where the error comes in ...
+        temp2 = reshape(temp1,n_rows,n_cols);
+        
         s.grid_handles = flipud(temp2);
-    %end
-    
+        s.tolerance = tolerance;
+        s.unique_col_positions = unique_col_positions;
+        s.unique_row_positions = unique_row_positions;
+		is_good = true;
+        break
+    catch ME
+    end
+end
+
+if ~is_good
+	error('Unable to find appropriate organization for handles')
+end
+
+%end
--- +sl/+plot/@subplotter/subplotter.m
+++ +sl/+plot/@subplotter/subplotter.m
@@ -207,76 +207,7 @@ classdef subplotter < sl.obj.display_class
     %Constructors
     %-----------------------------------------------
     methods (Static)
-        function obj = fromFigure(fig_handle,varargin)
-            %x Construct object from figure handle
-            %
-            %   sp = sl.plot.subplotter.fromFigure(fig_handle,varargin)
-            %
-            %   Attempts to create object from figure handle. Requires
-            %   trying to get subplot shape (unless shape is specified)
-            %
-            %   Optional Inputs
-            %   ---------------
-            %   shape : 2 element vector [n_rows, n_columns]
-            %   may_be_single : default false
-            %       If true, we may only have a single plot and not
-            %       really a subplot ...
-            %           Apparently this may not be needed ....
-            %
-            %   Improvements
-            %   ------------
-            %   1) Support partial subplots
-            
-            %Changed the calling form from:
-            %1) fig_handle, *shape
-            %to 
-            %2) fig_handle, varargin
-            if nargin == 2
-                add_shape = true;
-                temp_shape = varargin{1};
-                varargin(1) = [];
-            else
-                add_shape = false;
-            end
-            
-            in.shape = [];
-            in.may_be_single = false;
-            in = sl.in.processVarargin(in,varargin);
-            
-            if add_shape
-               in.shape = temp_shape; 
-            end
-            
-            if ~isvalid(fig_handle)
-                error('Invalid figure handle, figure likely closed')
-            end
-            
-            try
-                %Push this to getSubplotAxesHandles??
-                if ~isempty(in.shape)
-                    handles = findobj(gcf,'Type','axes');
-                    grid_handles = reshape(flip(handles),shape(1),shape(2));
-                else
-                    temp = sl.hg.figure.getSubplotAxesHandles(fig_handle);
-                    grid_handles = temp.grid_handles;
-                end
-                sz = size(grid_handles);
-            catch ME
-                %I saw this for a figure that was empty ...
-                %Also a partial figure ...
-                %
-                if in.may_be_single
-                    error('Case not yet handled')
-                   keyboard 
-                else
-                   rethrow(ME) 
-                end
-            end
-            
-            obj = sl.plot.subplotter(sz(1),sz(2),'clf',false);
-            obj.handles = num2cell(grid_handles);
-            obj.h_fig = fig_handle;
-        end
+        obj = fromFigure(fig_handle,varargin)
     end
     
     methods
