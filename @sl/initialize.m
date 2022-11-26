9258de7cf23a51db3e4d7e846cac479d0745305f
Jim Hokanson <jim.hokanson@gmail.com>
2022-11-26 14:30:11 -0600

    Update initialize.m
    adding documentation

--- @sl/initialize.m
+++ @sl/initialize.m
@@ -18,10 +18,23 @@ end
 %repo_root - points to folder containing +sl, not +sl itself
 
 %Why am I changing the directory????
+%
+%   I believe this is just part of starting out that you are at the root
+%   of all the MATLAB repos. I'm not thrilled with it but probably
+%   won't change it at this point.
+%
 cd(fileparts(repo_root));
+
+
+%Module initialization
+%---------------------
+sl.modules.initialize();
+
+
 
 %Adding other directories
 %---------------------------------------------
+%This directory holds onto functions/scripts that are called directly
 gnf_dir = fullfile(repo_root,'global_namespace_functions');
 temp_fcns_dir = fullfile(gnf_dir,'temp__dynamic_functions');
 addpath(gnf_dir)
@@ -30,18 +43,19 @@ addpath(temp_fcns_dir)
 fex_dir = fullfile(repo_root,'fex');
 addpath(fex_dir)
 
-sl.modules.initialize();
-
-
-%---------------------------------------------
 
 %runc support
+%---------------------------------------------
 %TODO: I don't think this is needed anymore
 temp_runc_file_path = fullfile(gnf_dir,'z_runc_exec_file.m');
 sl.io.fileWrite(temp_runc_file_path,' ');
 
 
-
+%Sigar
+%----------------------------------------------------
+%sigar - used for memory info
+%
+%   sl.os.sys_mem_info
 
 %TODO: I added a non-existant path and javaaddpath didn't say anything
 %I also don't want to add if it is already added
@@ -49,19 +63,4 @@ sl.io.fileWrite(temp_runc_file_path,' ');
 java_jar_path = fullfile(repo_root,'src','java');
 sigar_path = fullfile(java_jar_path,'sigar','sigar.jar');
 javaaddpath(sigar_path);
-
-%javaaddpath('C:\D\repos\matlab_git\mat_std_lib\src\java\sigar\sigar.jar')
-
-
-
-%TODO: Change this ...
-%
-% %Let's first check that the class is not in the static path
-% if NEURON.comm_obj.java_comm_obj.validate_installation
-%     %Do nothing
-% else
-    %javaaddpath(sl.java.read_buffered_stream);
-    
-% end
-
 end
