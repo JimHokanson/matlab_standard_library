c110aa46b519e0c037d8c529333c3bb9b5f6b94c
Jim Hokanson <jim.hokanson@gmail.com>
2022-11-26 15:18:52 -0600

    Update getOptionalParameter.m
    updated documentation and fixed a bug

--- +sl/+in/getOptionalParameter.m
+++ +sl/+in/getOptionalParameter.m
@@ -5,27 +5,33 @@ function [is_found,value,options] = getOptionalParameter(options,name,varargin)
 %   I originally wrote this in the case in which I needed to do something
 %   specific if a 'Parent' optional input had been passed into a function.
 %
+%   Matches are case-insensitive if options is a 
+%
 %   Inputs
 %   ------
 %   options : struct or cell
-%       Contains property/value pairs
+%       - struct
+%           - field is property, value is value
+%       - cell
+%           - pairs of property/values {prop1 value1 prop2 value2 etc.}
 %   name : string
 %       Name of the property whose value should be found
 %
 %   Optional Inputs
 %   ---------------
-%   default : (default [])
+%   default : default []
 %       Default value if not found. Using this can avoid an if statement
 %       in the caller.
-%   remove : (default false)
+%   remove : default false
 %       If true, the value is removed if found.
 %
 %   Outputs
 %   -------
 %   is_found : logical
 %       Whether or not the name was present in the options
-%   value : 
+%   value :
 %       The value of the specified property option.
+%   options :
 %
 %   Examples
 %   --------
@@ -38,16 +44,16 @@ in.remove = false;
 in = sl.in.processVarargin(in,varargin);
 
 value = in.default;
-if isstruct(name)
-    is_found = isfield(options,name);
+if isstruct(options)
+    [is_found,name] = sl.struct.isfieldi(options,name);
     if is_found
         value = options.(name);
-    	if in.remove
+        if in.remove
             options = rmfield(options,name);
         end
     end
 else
-
+    
     %TODO: Does this behavior need to change???
     %Are multiple matches an error or is the last one typically used ...
     %
@@ -55,10 +61,10 @@ else
     I = find(strcmpi(options(1:2:end),name),1);
     is_found = ~isempty(I);
     if is_found
-       value = options{2*I};
+        value = options{2*I};
         if in.remove
             options([2*I - 1, 2*I]) = [];
         end
     end
-
+    
 end
