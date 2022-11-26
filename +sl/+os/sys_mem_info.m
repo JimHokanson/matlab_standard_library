12d0344f79eabf6f90607640df38d34a9f9f6ffc
Jim Hokanson <jim.hokanson@gmail.com>
2022-11-25 22:57:16 -0600

    Update sys_mem_info.m
    updated documentation

--- +sl/+os/sys_mem_info.m
+++ +sl/+os/sys_mem_info.m
@@ -9,22 +9,27 @@ classdef sys_mem_info
     %
     %   Old link:
     %   http://cpansearch.perl.org/src/DOUGM/hyperic-sigar-1.6.3-src/docs/javadoc/org/hyperic/sigar/Mem.html
-    
-    
-    %TODO: Make total = used + free
-    %TODO: Make gb variables
+    %
+    %   sigar looks outdated
+    %
+    %   I wanted a nice way of assessing free memory that was cross
+    %   platform. Sigar seemed useful but looks outdated. Not sure it
+    %   works properly on modern systems.
     
     properties
-        %total
+        total
         used
         free
+        used_percent
+        free_percent
         actual_used
         actual_free
     end
     
     properties
-       used_GB
-       free_GB
+        used_GB
+        free_GB
+        total_GB
     end
     
     methods
@@ -33,15 +38,22 @@ classdef sys_mem_info
             %   m = sl.os.sys_mem_info()
             %
             
-           s = org.hyperic.sigar.Sigar;
-           m = s.getMem;
-           
-           %obj.total = m.getRam;
-           obj.used = m.getUsed;
-           obj.free = m.getFree;
-           obj.actual_used = m.getActualUsed;
-           obj.actual_free = m.getActualFree;
-           
+            s = org.hyperic.sigar.Sigar;
+            m = s.getMem;
+            
+            %obj.total = m.getRam;
+            obj.used = m.getUsed;
+            obj.free = m.getFree;
+            obj.used_percent = m.getUsedPercent;
+            obj.free_percent = m.getFreePercent;
+            %documentation?
+            obj.actual_used = m.getActualUsed;
+            obj.actual_free = m.getActualFree;
+            %k,m,g
+            obj.used_GB = obj.used/(1024^3);
+            obj.free_GB = obj.free/(1024^3);
+            obj.total_GB = obj.used_GB + obj.free_GB;
+            
         end
     end
     
