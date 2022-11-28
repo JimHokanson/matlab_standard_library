b0d502f3ae81df01d78405307878e262f9c7a3db
Jim Hokanson <jim.hokanson@gmail.com>
2022-11-27 22:17:31 -0600

    Update simpleThreshold.m
    documentation updates

--- +sci/+time_series/+calculators/@event_calculators/simpleThreshold.m
+++ +sci/+time_series/+calculators/@event_calculators/simpleThreshold.m
@@ -8,7 +8,8 @@ function results = simpleThreshold(data_objs,threshold_value,look_for_positive,v
 %               threshold_result,1,epoch_value,varargin)
 %
 %   In the 2nd case, we use the epochs to generate a mask. This class
-%   then serves to further refine the epochs.
+%   then serves to further refine the epochs. This is useful as sometimes
+%   the order of operations can be difficult to infer.
 %
 %
 %   Inputs
@@ -21,7 +22,19 @@ function results = simpleThreshold(data_objs,threshold_value,look_for_positive,v
 %   threshold_result : sci.time_series.event_results.simple_threshold_results
 %   epoch_value : logical
 %       If true, the epochs generate a positive mask. Otherwise, the
-%       non-epoch spans are true, and the 
+%       non-epoch spans are true, and the epochs are false. In other words,
+%       this is a way of setting all non-epochs to epochs and epochs to
+%       nothing.
+%           
+%                           epoch         epoch
+%                        ---|||||---------||||||-----
+%
+%                           epoch         epoch
+%          true          ---|||||---------||||||-----
+%
+%                        epoch   epoch          epoch
+%          false         |||-----|||||||||------|||||
+%
 %
 %   Optional Inputs
 %   ---------------
@@ -35,8 +48,14 @@ function results = simpleThreshold(data_objs,threshold_value,look_for_positive,v
 %           This means the input value is true when between 0.5 and 1
 %           or between (or equal to) 2 and 3.
 %   allow_starting_true : (default true)
+%       Whether an epoch can start with the first sample. If false
+%       the first sets of trues that would normally define an epoch
+%       are dropped.
 %   allow_ending_true: (default true)
+%       Similar to 'allow_starting_true' but for the end.
+%
 %   allow_border_trues: (default true)
+%       if set overrides allow_starting_true and allow_ending_true
 %
 %   original_data : 
 %       This can pass an 'original dataset' to the results for later
@@ -57,22 +76,34 @@ function results = simpleThreshold(data_objs,threshold_value,look_for_positive,v
 %       behavior is to allow any data value that exceeds a threshold. This
 %       essentially allows a between comparison, rather than just an
 %       exceeds/threshold comparision.
+%
+%   Below options get implemented before and after the other filters. If a
+%   different order is needed you can run the results, and then call the
+%   function again:
+%       result = simpleThreshold(result,1,epoch_value)
+%
 %   join_time : (default, not used)
 %       Intertimes that are less than this value allow are combined. This
 %       is done before the other filters ...
 %   join_after_time : (default, not used)
 %       Same as 'join_time', but this is run after the other filters (i.e.
 %       after min max filters)
-%   epoch_value : logical (default true)
-%       This is only for when a previous threshold result is passed in.
-%       A value of true indicates that the output should be true based on
-%       when the intput is true. A value of false indicates that the output
-%       should be true when the input is false.
 %
 %   Outputs
 %   -------
 %   results : sci.time_series.event_results.simple_threshold_results
 %       One object for every input data object is returned.
+%
+%
+%   Examples
+%   --------
+%   d = sci.time_series.data.example(2);
+%   d = abs(d);
+%   c = d.calculators;
+%   result = c.eventz.simpleThreshold(d,2,true);
+%   plot(result)
+%   
+%
 %
 %   Improvements
 %   -----------------------------------------------------------------------
@@ -181,15 +212,19 @@ for iObj = 1:length(data_objs)
     if ~isempty(in.min_time)
         mask2(bti.true_durations < in.min_time) = false;
     end
+    
     if ~isempty(in.min_samples)
         mask2(bti.true_sample_durations < in.min_samples) = false;
     end
+    
     if ~isempty(in.max_time)
         mask2(bti.true_durations > in.max_time) = false;
     end
+    
     if ~isempty(in.max_samples)
         mask2(bti.true_sample_durations > in.max_samples) = false;
     end
+    
     if ~isempty(in.min_intertime)
         %TODO: We could maybe improve the speed by doing a search over
         %valid Is first find(mask2)
@@ -248,8 +283,6 @@ for iObj = 1:length(data_objs)
     temp.threshold_end_times   = bti.true_end_times(mask2);
     temp.threshold_end_I       = bti.true_end_indices(mask2);
     temp.n_samples = length(mask);
-    
-    
     
     results_ca{iObj} = temp;
 end
