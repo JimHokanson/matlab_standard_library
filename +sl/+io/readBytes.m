763cce53957f60c4a2997cd9b6604f8d193cf262
Jim Hokanson <jim.hokanson@gmail.com>
2026-04-09 17:01:00 -0500

    Update readBytes.m
    made file specification optional

--- +sl/+io/readBytes.m
+++ +sl/+io/readBytes.m
@@ -1,11 +1,22 @@
 function bytes = readBytes(file_path)
 %x Read a file as bytes
 %
-%   bytes = sl.io.readBytes(file_path)
+%   bytes = sl.io.readBytes(*file_path)
 %
 %   See Also
 %   --------
 %   sl.io.fileRead
+
+if nargin == 0
+    [filename, pathname] = uigetfile('*', 'Pick a file');
+    if isequal(filename,0)
+        bytes = [];
+        return
+    end
+    file_path = fullfile(pathname,filename);
+end
+
+
 
 bytes = sl.io.fileRead(file_path,'*uint8');
 
